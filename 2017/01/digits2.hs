e06b06ad8bf46939b9a14c1b510258b96f240ea5
derrickturk <dwt@terminusdatascience.com>
2018-11-30 10:53:07 -0600

    Solve part 2

---
+++ 2017/01/digits2.hs
@@ -0,0 +1,21 @@
+import Data.Char (isDigit, ord)
+
+pairs :: [Int] -> [(Int, Int)]
+pairs xs = let steps = length xs `div` 2 in
+  zip xs (drop steps $ cycle xs)
+
+checksum :: [Int] -> Int
+checksum = sum . fmap fst . filter (uncurry (==)) . pairs
+
+parse :: String -> Maybe [Int]
+parse = traverse parseChar where
+  parseChar c
+    | isDigit c = Just $ ord c - ord '0'
+    | otherwise = Nothing
+
+main :: IO ()
+main = do
+  input <- parse <$> getLine
+  case input of
+    Just digits -> print $ checksum digits
+    Nothing -> putStrLn "invalid input"
