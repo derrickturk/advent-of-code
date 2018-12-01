3731df6c0e1ee55ccf6a7bf9d3fac809c4677bed
derrickturk <dwt@terminusdatascience.com>
2018-12-01 09:13:53 -0600

    2017 day 5.

---
+++ 2017/05/hsjump/src/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import Control.Monad.ST
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Text.Read (readMaybe)
+
+makeMemory :: [Int] -> ST s (MV.MVector s Int)
+makeMemory = V.thaw . V.fromList
+
+runInstruction :: (MV.MVector s Int) -> Int -> ST s Int
+runInstruction vec ip = do
+  jump <- MV.read vec ip
+  MV.modify vec (+ 1) ip
+  pure $ ip + jump
+
+stepsToHalt :: [Int] -> ST s Int
+stepsToHalt insts = do
+  mem <- makeMemory insts
+  go mem 0 0
+  where
+  go mem count ip = do
+    if ip < 0 || ip >= MV.length mem
+      then pure count
+      else runInstruction mem ip >>= go mem (count + 1)
+
+parseMemory :: String -> Maybe [Int]
+parseMemory = traverse readMaybe . words
+
+main :: IO ()
+main = do
+  input <- parseMemory <$> getContents
+  case input of
+    Nothing -> putStrLn "invalid input"
+    Just mem -> print $ runST $ stepsToHalt mem
