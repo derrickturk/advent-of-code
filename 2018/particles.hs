015eba6e93c445edadbb970af86d4c1b7425b912
derrickturk <dwt@terminusdatascience.com>
2018-12-10 10:03:41 -0600

    Fun with ascii art.

--- 2018/particles.hs
+++ 2018/particles.hs
@@ -1,5 +1,7 @@
+import System.Environment (getArgs)
 import Text.Read (readMaybe)
 import Data.List (stripPrefix, foldl')
+import Data.Maybe (fromMaybe)
 import qualified Data.Set as S
 
 newtype Position = Position { getPosition :: (Int, Int) }
@@ -26,16 +28,20 @@ particlePosition t (Particle p v) = positionAt t p v
 positions :: [Particle] -> [[Position]]
 positions ps = (\t -> particlePosition (Time t) <$> ps) <$> enumFrom 0
 
-printPositions :: [Position] -> String
+printPositions :: [Position] -> Maybe String
 printPositions ps =
-  let (x_min, x_max, y_min, y_max, set) = foldl' update (0, 0, 0, 0, S.empty) ps
+  let (x_min, x_max, y_min, y_max, set) =
+        foldl' update (maxBound, minBound, maxBound, minBound, S.empty) ps
       update (x_min, x_max, y_min, y_max, s) (Position (x, y)) =
         (min x_min x, max x_max x, min y_min y, max y_max y, S.insert (x, y) s)
       makeRow x_min x_max s y = fmap (drawPoint s y) (enumFromTo x_min x_max)
       drawPoint s y x = if S.member (x, y) s
         then '#'
         else ' '
-  in unlines $ fmap (makeRow x_min x_max set) (enumFromTo y_min y_max)
+  in if (x_max - x_min < 1000 && y_max - y_min < 1000) -- 'density heuristic'
+    then Just $ unlines $ (makeRow x_min x_max set) <$> (enumFromTo y_min y_max)
+    else Nothing
+  -- in unlines $ (makeRow x_min x_max set) <$> (enumFromTo y_min y_max)
 
 parseParticle :: String -> Maybe Particle
 parseParticle s = do
@@ -59,8 +65,17 @@ parseParticle s = do
 
 main :: IO ()
 main = do
+  args <- getArgs
+  let (nDrop, nTake) = case args of
+        [] -> (0, 5)
+        [n] -> (0, read n)
+        (d:t:_) -> (read d, read t)
   input <- traverse parseParticle . lines <$> getContents
   case input of
     Nothing -> putStrLn "invalid input"
     Just particles -> do
-      mapM_ (putStrLn . printPositions) $ take 5 $ positions particles
+      mapM_ showLine $ take nTake $ drop nDrop $ zip [0..] (positions particles)
+  where
+    showLine (i, pos) = do
+      print i
+      putStrLn $ fromMaybe "<too sparse>" $ printPositions pos
