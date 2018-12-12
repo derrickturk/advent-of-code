0d1d5ac86210fe648a6c095e7559b43b5cfe5bbf
derrickturk <dwt@terminusdatascience.com>
2018-12-12 11:04:11 -0600

    A dumb solution to a dumb math problem.

--- 2018/12/hsplants/src/Main.hs
+++ 2018/12/hsplants/src/Main.hs
@@ -4,10 +4,30 @@ import qualified Data.Text.IO as TIO
 import Plants
 import Parse
 
+differences :: [Int] -> [Int]
+differences xs = 0:zipWith (-) (tail xs) xs
+
+dropTilConvergence :: Eq a => (b -> a) -> [b] -> [b]
+dropTilConvergence _ [] = []
+dropTilConvergence _ [_] = []
+dropTilConvergence f whole@(x:rest@(y:_))
+  | f x == f y = whole
+  | otherwise = dropTilConvergence f rest
+
+estimateFuture :: [Int] -> Int -> Int
+estimateFuture xs i =
+  let third (_, _, x) = x
+      trend = dropTilConvergence third $ zip3 [0..] xs (differences xs)
+      (j, val, diff) = head trend
+  in val + (i - j) * diff
+
 main :: IO ()
 main = do
   parsed <- parse (only setup) "stdin" <$> TIO.getContents
   case parsed of
     Left e -> putStrLn $ parseErrorPretty e
-    Right (ps, rules) -> print $ sum $
-      plantPots $ iterateRules (compileRules rules) ps !! 20
+    Right (ps, rules) -> do
+      let steps = iterateRules (compileRules rules) ps
+          filledPots = sum . plantPots <$> steps
+      print $ filledPots !! 20
+      print $ estimateFuture filledPots 50000000000
