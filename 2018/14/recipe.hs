a71e30e48116133361fbc1efbff6ab3b0c7cfc4e
derrickturk <dwt@terminusdatascience.com>
2018-12-16 15:47:15 -0600

    is that faster?

--- 2018/14/recipe.hs
+++ 2018/14/recipe.hs
@@ -1,33 +1,56 @@
+{-# LANGUAGE StrictData #-}
+
+import Control.Monad.ST
+import Data.STRef
 import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
 import Data.List (findIndex)
 import Data.Int (Int8)
 
-type Recipes = V.Vector Int8
+type Recipes s = MV.MVector s Int8
+
+data GameState s = GameState { scores :: Recipes s
+                             , i1 :: STRef s Int
+                             , i2 :: STRef s Int
+                             }
 
 digits :: (Show a, Read a) => a -> [a]
 digits = fmap (read . pure) . show
 
-combine :: Recipes -> Int -> Int -> Recipes
-combine vec i j = V.fromList $ digits $ vec V.! i + vec V.! j
+initial :: ST s (GameState s)
+initial = GameState <$> (V.thaw $ V.fromList [3, 7])
+                    <*> newSTRef 0
+                    <*> newSTRef 1
 
-update :: Recipes -> Int -> Int -> (Recipes, Int, Int)
-update vec i j =
-  let vec' = vec <> combine vec i j
-      i' = (i + 1 + fromIntegral (vec V.! i)) `mod` V.length vec'
-      j' = (j + 1 + fromIntegral (vec V.! j)) `mod` V.length vec'
-  in (vec', i', j')
+stepGame :: GameState s -> ST s (GameState s)
+stepGame (GameState scores i1 i2) = do
+  s1 <- readSTRef i1 >>= MV.read scores
+  s2 <- readSTRef i2 >>= MV.read scores
+  let combined = s1 + s2
+  scores' <- if combined >= 10
+    then do
+      s' <- MV.grow scores 2
+      MV.write s' (MV.length s' - 2) 1
+      MV.write s' (MV.length s' - 1) (combined - 10)
+      pure s'
+    else do
+      s' <- MV.grow scores 1
+      MV.write s' (MV.length s' - 1) combined
+      pure s'
+  modifySTRef i1 $ (`mod` MV.length scores') . (+ (1 + fromIntegral s1))
+  modifySTRef i2 $ (`mod` MV.length scores') . (+ (1 + fromIntegral s2))
+  pure $ GameState scores' i1 i2
+{-# INLINE stepGame #-}
+
+runTilLength :: Int -> GameState s -> ST s (GameState s)
+runTilLength n s
+  | MV.length (scores s) >= n = pure s
+  | otherwise = stepGame s >>= runTilLength n
 
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (x, y, z) = f x y z
 
-runTilLength :: Recipes -> Int -> Int -> Int -> (Recipes, Int, Int)
-runTilLength vec i j n
-  | V.length vec >= n = (vec, i, j)
-  | otherwise = let (vec', i', j') = update vec i j in runTilLength vec' i' j' n
-
-initial :: Recipes
-initial = V.fromList [3, 7]
-
+{-
 subseqIndex :: Recipes -> Recipes -> Maybe Int
 subseqIndex vec sub
   | V.length sub > V.length vec = Nothing
@@ -44,6 +67,7 @@ runTilSubseq :: Recipes
 runTilSubseq vec i j sub = case subseqIndex vec sub of
   Just n -> (vec, i, j, n)
   _ -> let (vec', i', j') = update vec i j in runTilSubseq vec' i' j' sub
+-}
 
 fst3 :: (a, b, c) -> a
 fst3 (x, _, _) = x
@@ -51,14 +75,18 @@ fst3 (x, _, _) = x
 fourth :: (a, b, c, d) -> d
 fourth (_, _, _, x) = x
 
-solvePt1 :: Int -> Recipes
-solvePt1 n = V.slice n 10 (fst3 $ runTilLength initial 0 1 (n + 10))
+solvePt1 :: Int -> ST s [Int8]
+solvePt1 n = do
+  final <- initial >>= runTilLength (n + 10)
+  V.toList <$> (V.freeze $ MV.slice n 10 $ scores final)
 
+{-
 solvePt2 :: Recipes -> Int
 solvePt2 = fourth . runTilSubseq initial 0 1
+-}
 
 main :: IO ()
 main = do
   input <- read <$> getLine
-  putStrLn $ concatMap show $ V.toList (solvePt1 input)
-  print $ solvePt2 $ V.fromList $ fromIntegral <$> digits input
+  putStrLn $ concatMap show $ runST $ solvePt1 input
+  -- print $ solvePt2 $ V.fromList $ fromIntegral <$> digits input
