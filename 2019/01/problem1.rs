5e67a0525eb06ae72b84839b6273ae99a6fdeb06
derrickturk <dwt@terminusdatascience.com>
2019-12-02 08:08:51 -1000

    benchmark

--- 2019/01/problem1.rs
+++ 2019/01/problem1.rs
@@ -1,5 +1,9 @@
-use std::{env, io, mem};
-use std::io::BufRead;
+use std::{
+    env,
+    io::{self, BufRead},
+    mem,
+    time::Instant,
+};
 
 struct Iterate<T, F> {
     next: T,
@@ -32,16 +36,36 @@ fn total_fuel(mass: i32) -> i32 {
     iterate(fuel, mass).skip(1).take_while(|f| *f > 0).sum()
 }
 
+#[inline]
+fn total_fuel_proc(mass: i32) -> i32 {
+    let mut total = 0;
+    let mut incr = fuel(mass);
+    while incr > 0 {
+        total += incr;
+        incr = fuel(incr);
+    }
+    total
+}
+
 fn main() {
     if let Some(prob) = env::args().skip(1).next() {
         let stdin = io::stdin();
         let masses = stdin.lock().lines()
             .map(|l| l.unwrap().trim_end().parse::<i32>().unwrap());
-        match prob.as_str() {
-            "1" => println!("{}", masses.map(fuel).sum::<i32>()),
-            "2" => println!("{}", masses.map(total_fuel).sum::<i32>()),
-            _ => eprintln!("Usage: problem1 1|2 <input.txt"),
-        }
+        let begin = Instant::now();
+        let result = match prob.as_str() {
+            "1" => masses.map(fuel).sum::<i32>(),
+            "2f" => masses.map(total_fuel).sum::<i32>(),
+            "2p" => masses.map(total_fuel_proc).sum::<i32>(),
+            _ => {
+                eprintln!("Usage: problem1 1|2 <input.txt");
+                return;
+            },
+        };
+        let elapsed = begin.elapsed();
+        println!("{}", result);
+        println!("in {} ms", (elapsed.as_secs() * 1000) as f64 +
+            (elapsed.subsec_nanos() as f64 / 1e6));
     } else {
         eprintln!("Usage: intcode 1|2 <input.txt");
     }
