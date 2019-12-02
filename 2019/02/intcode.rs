c8cb2b096a40f03d3576d00e5a844ac1aad2cae2
derrickturk <dwt@terminusdatascience.com>
2019-12-01 21:02:20 -1000

    Day 2 part 2.

--- 2019/02/intcode.rs
+++ 2019/02/intcode.rs
@@ -1,5 +1,6 @@
 use std::{
     io,
+    env,
     convert::TryInto,
 };
 
@@ -26,28 +27,25 @@ fn indirect_address(program: &mut [i32], ptr: usize)
 }
 
 impl OpCode {
-    fn parse(program: &mut [i32], ip: usize)
-      -> Result<(OpCode, usize), IntCodeError> {
+    fn parse(program: &mut [i32], ip: usize) -> Result<OpCode, IntCodeError> {
         match *program.get(ip).ok_or(IntCodeError::OutOfProgram)? {
             1 => {
-                let opcode = OpCode::Add(
+                Ok(OpCode::Add(
                     indirect_address(program, ip + 1)?,
                     indirect_address(program, ip + 2)?,
                     indirect_address(program, ip + 3)?,
-                );
-                Ok((opcode, ip + 4))
+                ))
             },
 
             2 => {
-                let opcode = OpCode::Mul(
+                Ok(OpCode::Mul(
                     indirect_address(program, ip + 1)?,
                     indirect_address(program, ip + 2)?,
                     indirect_address(program, ip + 3)?,
-                );
-                Ok((opcode, ip + 4))
+                ))
             },
 
-            99 => Ok((OpCode::Halt, ip)),
+            99 => Ok(OpCode::Halt),
 
             n => Err(IntCodeError::UnknownOpCode(n)),
         }
@@ -80,17 +78,25 @@ impl OpCode {
 
         Ok(())
     }
+
+    fn ip_step(&self) -> usize {
+        match self {
+            OpCode::Add(..) => 4,
+            OpCode::Mul(..) => 4,
+            OpCode::Halt => 1,
+        }
+    }
 }
 
 fn execute(program: &mut [i32]) -> Result<(), IntCodeError> {
     let mut ip = 0;
     loop {
-        let (opcode, next_ip) = OpCode::parse(program, ip)?;
-        ip = next_ip;
+        let opcode = OpCode::parse(program, ip)?;
         match opcode {
             OpCode::Halt => return Ok(()),
             _ => opcode.eval(program)?,
         }
+        ip += opcode.ip_step();
     }
 }
 
@@ -102,13 +108,43 @@ fn problem1(program: &mut [i32]) -> Result<(), IntCodeError> {
     Ok(())
 }
 
-fn main() -> Result<(), IntCodeError> {
-    let mut program = String::new();
-    io::stdin().read_line(&mut program)
-        .expect("failed to read program from stdin");
-    let mut program: Vec<i32> = program.trim_end().split(',')
-        .map(|word| word.parse().map_err(|_| IntCodeError::ParseError))
-        .collect::<Result<_, _>>()?;
+fn problem2(program: &[i32]) {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut this_prog = program.to_owned();
+            this_prog[1] = noun;
+            this_prog[2] = verb;
 
-    problem1(&mut program[..])
+            if let Ok(_) = execute(&mut this_prog[..]) {
+                if this_prog[0] == 19690720 {
+                    println!("{}", 100 * noun + verb);
+                    return;
+                }
+            }
+        }
+    }
+    eprintln!("no solution found.");
+}
+
+fn main() -> Result<(), IntCodeError> {
+    if let Some(prob) = env::args().skip(1).next() {
+        let mut program = String::new();
+        io::stdin().read_line(&mut program)
+            .expect("failed to read program from stdin");
+        let mut program: Vec<i32> = program.trim_end().split(',')
+            .map(|word| word.parse().map_err(|_| IntCodeError::ParseError))
+            .collect::<Result<_, _>>()?;
+
+        match prob.as_str() {
+            "1" => problem1(&mut program[..]),
+            "2" => Ok(problem2(&program[..])),
+            _ => {
+                eprintln!("Usage: intcode 1|2 <input.txt");
+                Ok(())
+            }
+        }
+    } else {
+        eprintln!("Usage: intcode 1|2 <input.txt");
+        Ok(())
+    }
 }
