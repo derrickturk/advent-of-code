1c5a79a5a69323bf38b3d9d7fb81d44cd32a41cc
derrickturk <dwt@terminusdatascience.com>
2019-12-10 23:16:36 -0600

    WIP: labels for disassembly.

--- 2019/09/intcode/src/asm.rs
+++ 2019/09/intcode/src/asm.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::hash_map::HashMap,
+    io::{self, Write},
+};
+
 use super::{
     IntCodeError,
     ReadOperand,
@@ -7,74 +12,299 @@ use super::{
     ExpandoVec,
 };
 
+type LabelMap = HashMap<usize, String>;
+
+#[derive(Debug)]
+pub enum DisAsmError {
+    IntCodeError(IntCodeError),
+    IOError(io::Error),
+}
+
+impl From<IntCodeError> for DisAsmError {
+    fn from(other: IntCodeError) -> Self {
+        DisAsmError::IntCodeError(other)
+    }
+}
+
+impl From<io::Error> for DisAsmError {
+    fn from(other: io::Error) -> Self {
+        DisAsmError::IOError(other)
+    }
+}
+
 trait DisAsm {
-    fn disassemble(&self) -> String;
+    fn disassemble_at(&self, writer: &mut impl Write,
+          labels: &LabelMap, ip: usize) -> Result<(), DisAsmError>;
 }
 
 impl DisAsm for WriteOperand {
-    fn disassemble(&self) -> String {
-        match *self {
-            WriteOperand::Position(ptr) => format!("{}", ptr),
-            WriteOperand::Relative(offset) => format!("({})", offset),
+    fn disassemble_at(&self, writer: &mut impl Write,
+          labels: &LabelMap, ip: usize) -> Result<(), DisAsmError> {
+        if let Some(lbl) = labels.get(&ip) {
+            write!(writer, "{}: ", lbl)?;
         }
+
+        match *self {
+            WriteOperand::Position(ptr) => match labels.get(&ptr) {
+                None => write!(writer, "{}", ptr)?,
+                Some(lbl) => write!(writer, "{}", lbl)?,
+            }
+            WriteOperand::Relative(offset) => write!(writer, "({})", offset)?,
+        };
+
+        Ok(())
     }
 }
 
 impl DisAsm for ReadOperand {
-    fn disassemble(&self) -> String {
+    fn disassemble_at(&self, writer: &mut impl Write,
+          labels: &LabelMap, ip: usize) -> Result<(), DisAsmError> {
         match *self {
-            ReadOperand::Immediate(n) => format!("${}", n),
-            ReadOperand::Memory(op) => op.disassemble(),
+            ReadOperand::Immediate(n) => {
+                if let Some(lbl) = labels.get(&ip) {
+                    write!(writer, "{}: ", lbl)?;
+                }
+
+                write!(writer, "${}", n)?;
+                Ok(())
+            },
+
+            ReadOperand::Memory(op) => op.disassemble_at(writer, labels, ip),
         }
     }
 }
 
 impl DisAsm for OpCode {
-    fn disassemble(&self) -> String {
+    fn disassemble_at(&self, writer: &mut impl Write,
+          labels: &LabelMap, ip: usize) -> Result<(), DisAsmError> {
+        if let Some(lbl) = labels.get(&ip) {
+            write!(writer, "{}: ", lbl)?;
+        }
+
         match self {
-            OpCode::Add(src1, src2, dst) => format!("add {}, {}, {}",
-                src1.disassemble(), src2.disassemble(), dst.disassemble()),
-            OpCode::Mul(src1, src2, dst) => format!("add {}, {}, {}",
-                src1.disassemble(), src2.disassemble(), dst.disassemble()),
-            OpCode::Input(dst) => format!("in {}", dst.disassemble()),
-            OpCode::Output(src) => format!("out {}", src.disassemble()),
-            OpCode::JmpTrue(cnd, dst) => format!("jnz {}, {}",
-                cnd.disassemble(), dst.disassemble()),
-            OpCode::JmpFalse(cnd, dst) => format!("jz {}, {}",
-                cnd.disassemble(), dst.disassemble()),
-            OpCode::Less(src1, src2, dst) => format!("lt {}, {}, {}",
-                src1.disassemble(), src2.disassemble(), dst.disassemble()),
-            OpCode::Eql(src1, src2, dst) => format!("eq {}, {}, {}",
-                src1.disassemble(), src2.disassemble(), dst.disassemble()),
-            OpCode::AdjustRelBase(offset) => format!("rel {}",
-                offset.disassemble()),
-            OpCode::Halt => format!("hlt"),
+            OpCode::Add(src1, src2, dst) => {
+                write!(writer, "add ")?;
+                src1.disassemble_at(writer, labels, ip + 1)?;
+                write!(writer, ", ")?;
+                src2.disassemble_at(writer, labels, ip + 2)?;
+                write!(writer, ", ")?;
+                dst.disassemble_at(writer, labels, ip + 3)?;
+            },
+
+            OpCode::Mul(src1, src2, dst) => {
+                write!(writer, "mul ")?;
+                src1.disassemble_at(writer, labels, ip + 1)?;
+                write!(writer, ", ")?;
+                src2.disassemble_at(writer, labels, ip + 2)?;
+                write!(writer, ", ")?;
+                dst.disassemble_at(writer, labels, ip + 3)?;
+            },
+
+            OpCode::Input(dst) => {
+                write!(writer, "in ")?;
+                dst.disassemble_at(writer, labels, ip + 1)?;
+            },
+
+            OpCode::Output(src) => {
+                write!(writer, "out ")?;
+                src.disassemble_at(writer, labels, ip + 1)?;
+            },
+
+            OpCode::JmpTrue(cnd, dst) => {
+                write!(writer, "jnz ")?;
+                cnd.disassemble_at(writer, labels, ip + 1)?;
+                write!(writer, ", ")?;
+                dst.disassemble_at(writer, labels, ip + 2)?;
+            },
+
+            OpCode::JmpFalse(cnd, dst) => {
+                write!(writer, "jz ")?;
+                cnd.disassemble_at(writer, labels, ip + 1)?;
+                write!(writer, ", ")?;
+                dst.disassemble_at(writer, labels, ip + 2)?;
+            },
+
+            OpCode::Less(src1, src2, dst) => {
+                write!(writer, "lt ")?;
+                src1.disassemble_at(writer, labels, ip + 1)?;
+                write!(writer, ", ")?;
+                src2.disassemble_at(writer, labels, ip + 2)?;
+                write!(writer, ", ")?;
+                dst.disassemble_at(writer, labels, ip + 3)?;
+            },
+
+            OpCode::Eql(src1, src2, dst) => {
+                write!(writer, "eq ")?;
+                src1.disassemble_at(writer, labels, ip + 1)?;
+                write!(writer, ", ")?;
+                src2.disassemble_at(writer, labels, ip + 2)?;
+                write!(writer, ", ")?;
+                dst.disassemble_at(writer, labels, ip + 3)?;
+            },
+
+            OpCode::AdjustRelBase(offset) => {
+                write!(writer, "rel ")?;
+                offset.disassemble_at(writer, labels, ip + 1)?;
+            },
+
+            OpCode::Halt => {
+                write!(writer, "hlt")?;
+            }
+        };
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct DisAsmOpts {
+    pub line_addrs: bool,
+    pub labels: bool,
+}
+
+impl Default for DisAsmOpts {
+    fn default() -> Self {
+        Self {
+            line_addrs: false,
+            labels: false,
         }
     }
 }
 
-pub fn disassemble_memory(memory: &[i64]) -> Result<String, IntCodeError> {
-    let mut output = String::new();
+#[derive(Debug, Copy, Clone)]
+pub enum DisAsmStmt {
+    OpCode(OpCode),
+    Value(i64),
+}
+
+impl DisAsm for DisAsmStmt {
+    fn disassemble_at(&self, writer: &mut impl Write,
+          labels: &LabelMap, ip: usize) -> Result<(), DisAsmError> {
+        match self {
+            DisAsmStmt::OpCode(opcode) => {
+                opcode.disassemble_at(writer, labels, ip)?;
+            },
+
+            DisAsmStmt::Value(n) => {
+                if let Some(lbl) = &labels.get(&ip) {
+                    write!(writer, "{}: ", lbl)?;
+                }
+                write!(writer, "${}", n)?;
+            },
+        }
+
+        Ok(())
+    }
+}
+
+pub fn parse_memory(memory: &[i64], opts: &DisAsmOpts
+      ) -> Result<(Vec<(usize, DisAsmStmt)>, LabelMap), IntCodeError> {
     let mut state = ProgramState::<ExpandoVec>::from(memory.to_vec());
     let original_len = state.memory.0.len();
+
+    let mut stmts = Vec::new();
+    let mut labels = HashMap::new();
 
     while state.ip < original_len {
         let opcode = OpCode::parse_next(&mut state);
         match opcode {
             Ok(opcode) => {
+                if opts.labels {
+                    add_labels(&mut labels, &opcode);
+                }
+                stmts.push((state.ip, DisAsmStmt::OpCode(opcode)));
                 state.ip += opcode.width();
-                output += &opcode.disassemble();
-                output += "\n";
             },
 
             Err(IntCodeError::UnknownOpCode(n)) => {
+                stmts.push((state.ip, DisAsmStmt::Value(n)));
                 state.ip += 1;
-                output += &format!("${}\n", n);
             },
 
             Err(e) => return Err(e),
-        }
+        };
     }
 
-    Ok(output)
+    Ok((stmts, labels))
+}
+
+pub fn disassemble_memory(writer: &mut impl Write, memory: &[i64],
+      opts: &DisAsmOpts) -> Result<(), DisAsmError> {
+    let (stmts, labels) = parse_memory(memory, opts)?;
+    for (ip, stmt) in stmts {
+        if opts.line_addrs {
+            write!(writer, "{}\t", ip)?;
+        }
+
+        stmt.disassemble_at(writer, &labels, ip)?;
+        writeln!(writer)?;
+    }
+
+    Ok(())
+}
+
+fn add_labels(labels: &mut LabelMap, opcode: &OpCode) {
+    match opcode {
+        OpCode::Add(src1, src2, dst) => {
+            add_roperand_label(labels, src1);
+            add_roperand_label(labels, src2);
+            add_woperand_label(labels, dst);
+        },
+
+        OpCode::Mul(src1, src2, dst) => {
+            add_roperand_label(labels, src1);
+            add_roperand_label(labels, src2);
+            add_woperand_label(labels, dst);
+        },
+
+        OpCode::Input(dst) => {
+            add_woperand_label(labels, dst);
+        },
+
+        OpCode::Output(src) => {
+            add_roperand_label(labels, src);
+        },
+
+        OpCode::JmpTrue(cnd, dst) => {
+            add_roperand_label(labels, cnd);
+            add_roperand_label(labels, dst);
+        },
+
+        OpCode::JmpFalse(cnd, dst) => {
+            add_roperand_label(labels, cnd);
+            add_roperand_label(labels, dst);
+        },
+
+        OpCode::Less(src1, src2, dst) => {
+            add_roperand_label(labels, src1);
+            add_roperand_label(labels, src2);
+            add_woperand_label(labels, dst);
+        },
+
+        OpCode::Eql(src1, src2, dst) => {
+            add_roperand_label(labels, src1);
+            add_roperand_label(labels, src2);
+            add_woperand_label(labels, dst);
+        },
+
+        OpCode::AdjustRelBase(offset) => {
+            add_roperand_label(labels, offset);
+        },
+
+        OpCode::Halt => { },
+    };
+}
+
+#[inline]
+fn add_roperand_label(labels: &mut LabelMap, param: &ReadOperand) {
+    if let ReadOperand::Memory(WriteOperand::Position(n)) = *param {
+        labels.entry(n).or_insert_with(|| format!("lbl{}", n));
+    }
+}
+
+#[inline]
+fn add_woperand_label(labels: &mut LabelMap, param: &WriteOperand) {
+    if let WriteOperand::Position(n) = *param {
+        labels.entry(n).or_insert_with(|| format!("lbl{}", n));
+    }
 }
