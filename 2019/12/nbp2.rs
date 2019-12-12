809ac06c9b3a40f24649e22260eada6da5cfb533
derrickturk <dwt@terminusdatascience.com>
2019-12-12 14:44:26 -0600

    fuck this dumb problem, and fuck math, both of which I still don't understand

---
+++ 2019/12/nbp2.rs
@@ -0,0 +1,172 @@
+use std::{
+    collections::hash_set::HashSet,
+    cmp::Ordering,
+    ops::{Add, Sub, AddAssign, SubAssign},
+};
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+struct Vec3 {
+    x: i64,
+    y: i64,
+    z: i64,
+}
+
+impl Vec3 {
+    fn compare(self, other: Self) -> Self {
+        Self {
+            x: ordering_to_i64(self.x.cmp(&other.x)),
+            y: ordering_to_i64(self.y.cmp(&other.y)),
+            z: ordering_to_i64(self.z.cmp(&other.z)),
+        }
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.y + other.z,
+        }
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        self.x += other.x;
+        self.y += other.y;
+        self.z += other.z;
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.y - other.z,
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, other: Self) {
+        self.x -= other.x;
+        self.y -= other.y;
+        self.z -= other.z;
+    }
+}
+
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+struct Moon {
+    position: Vec3,
+    velocity: Vec3,
+}
+
+impl Moon {
+    fn at(x: i64, y: i64, z: i64) -> Self {
+        Self {
+            position: Vec3 { x, y, z, },
+            velocity: Vec3 { x: 0, y: 0, z: 0, },
+        }
+    }
+
+    // only apply to self! (loop is easier)
+    #[inline]
+    fn apply_gravity(&mut self, moon2: Moon) {
+        let cmp = self.position.compare(moon2.position);
+        self.velocity -= cmp;
+    }
+
+    #[inline]
+    fn step(&mut self) {
+        self.position += self.velocity;
+    }
+
+    #[inline]
+    fn potential(&self) -> i64 {
+        self.position.x.abs() + self.position.y.abs() + self.position.z.abs()
+    }
+
+    #[inline]
+    fn kinetic(&self) -> i64 {
+        self.velocity.x.abs() + self.velocity.y.abs() + self.velocity.z.abs()
+    }
+
+    #[inline]
+    fn total(&self) -> i64 {
+        self.potential() * self.kinetic()
+    }
+}
+
+#[inline]
+fn ordering_to_i64(ord: Ordering) -> i64 {
+    match ord {
+        Ordering::Less => -1,
+        Ordering::Equal => 0,
+        Ordering::Greater => 1,
+    }
+}
+
+fn main() {
+    let mut moons = [
+        Moon::at(-7, 17, -11),
+        Moon::at(9, 12, 5),
+        Moon::at(-9, 0, -4),
+        Moon::at(4, 6, 0),
+    ];
+
+    let moons_init = moons.clone();
+
+    let mut x_cycle = None;
+    let mut y_cycle = None;
+    let mut z_cycle = None;
+
+    let mut seen_x = HashSet::<Vec<(i64, i64)>>::new();
+    let mut seen_y = HashSet::<Vec<(i64, i64)>>::new();
+    let mut seen_z = HashSet::<Vec<(i64, i64)>>::new();
+
+    let mut iter = 0usize;
+    loop {
+        for i in 0..moons.len() {
+            for j in 0..moons.len() {
+                moons[i].apply_gravity(moons[j]);
+            }
+        }
+
+        for m in &mut moons {
+            m.step();
+        }
+
+        iter += 1;
+
+        if moons.iter().zip(moons_init.iter())
+            .all(|(m1, m2)| m1.position.x == m2.position.x
+                && m1.velocity.x == m2.velocity.x) {
+            x_cycle = Some(iter);
+        }
+
+        if moons.iter().zip(moons_init.iter())
+            .all(|(m1, m2)| m1.position.y == m2.position.y
+                && m1.velocity.y == m2.velocity.y) {
+            y_cycle = Some(iter);
+        }
+
+        if moons.iter().zip(moons_init.iter())
+            .all(|(m1, m2)| m1.position.z == m2.position.z
+                && m1.velocity.z == m2.velocity.z) {
+            z_cycle = Some(iter);
+        }
+
+        if x_cycle.is_some() && y_cycle.is_some() && z_cycle.is_some() {
+            break;
+        }
+    }
+
+    println!("lcm of {}, {}, {}",
+          x_cycle.unwrap(), y_cycle.unwrap(), z_cycle.unwrap());
+}
