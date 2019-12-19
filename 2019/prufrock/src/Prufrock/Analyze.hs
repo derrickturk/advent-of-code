e07dae67fbd87ccd96a00ac6091b6c59280415bd
derrickturk <dwt@terminusdatascience.com>
2019-12-19 16:22:59 -0600

    analyzer: build symbol table.

--- 2019/prufrock/src/Prufrock/Analyze.hs
+++ 2019/prufrock/src/Prufrock/Analyze.hs
@@ -2,15 +2,19 @@
 
 module Prufrock.Analyze (
     AnalysisError(..)
-  , FnEntry
   , FnTable
   , VarTable
-  , SymbolTable(..)
+  , SymbolTable
+  , symbolTableFns
+  , symbolTableGlobals
+  , symbolTableScopes
+  , getSymbolInfo
   , symbolTable
 ) where
 
 import qualified Data.Map.Strict as M
 
+import Control.Applicative
 import Control.Monad.State.Strict
 import Control.Monad.Except
 
@@ -22,35 +26,77 @@ data AnalysisError
   | TypeError Type Type
   deriving (Eq, Show)
 
-data FnEntry = FnEntry [(Ident, Type)] (Maybe Type)
-  deriving (Eq, Show)
-
-type FnTable = M.Map Ident (FnEntry, VarTable)
+type FnTable = M.Map Ident ([(Ident, Type)], (Maybe Type))
 type VarTable = M.Map Ident Type
 
 data SymbolTable
-  = SymbolTable { fnSymbols :: FnTable
-                , globalSymbols :: VarTable
+  = SymbolTable { symbolTableFns :: FnTable
+                , symbolTableGlobals :: VarTable
+                , symbolTableScopes :: M.Map Ident VarTable
                 } deriving (Eq, Show)
 
+data SymbolInfo
+  = SymbolFn [(Ident, Type)] (Maybe Type)
+  | SymbolVar Type
+  deriving (Eq, Show)
+
+getSymbolInfo :: SymbolTable -> Maybe Ident -> Ident -> Maybe SymbolInfo
+getSymbolInfo (SymbolTable fns globs locs) s name
+  | Just (args, ret) <- M.lookup name fns = Just $ SymbolFn args ret
+  | otherwise = case s of
+      Nothing -> SymbolVar <$> M.lookup name globs
+      Just fn -> SymbolVar <$>
+        (M.lookup name (locs M.! fn) <|> M.lookup name globs)
+
+insertScope :: (MonadState SymbolTable m, MonadError AnalysisError m)
+            => Maybe Ident
+            -> Ident
+            -> Type
+            -> m ()
+insertScope Nothing name ty = do
+  (SymbolTable fns globs locs) <- get
+  globs' <- M.alterF (\case
+     Nothing -> pure $ Just ty
+     Just _ -> throwError $ AlreadyDefined name) name globs
+  put $ SymbolTable fns globs' locs
+insertScope (Just fn) name ty = do
+  (SymbolTable fns globs locs) <- get
+  let loc = locs M.! fn
+  loc' <- M.alterF (\case
+     Nothing -> pure $ Just ty
+     Just _ -> throwError $ AlreadyDefined name) name loc
+  put $ SymbolTable fns globs (M.insert fn loc' locs)
+
 symbolTable :: Program -> Either AnalysisError SymbolTable
-symbolTable = flip execStateT (SymbolTable M.empty M.empty) . mapM_ visitItem
+symbolTable prog = do
+  fns <- fnTable prog
+  let initTab = SymbolTable fns M.empty (M.empty <$ fns)
+  execStateT (mapM_ visitItem prog) initTab
+  where
+    visitItem (StmtItem stmt) = visitStmt Nothing stmt
+    visitItem (FnDefItem (FnDef fn _ _ body)) = mapM_ (visitStmt $ Just fn) body
+    visitStmt s (Decl name ty _) = insertScope s name ty
+    visitStmt s (Assign e1 e2) = visitExpr s e1 >> visitExpr s e2
+    visitStmt s (AssignOp _ e1 e2) = visitExpr s e1 >> visitExpr s e2
+    visitStmt s (Input e) = visitExpr s e
+    visitStmt s (Output e) = visitExpr s e
+    visitStmt s (Return e) = visitExpr s e
+    visitExpr s (Var name) = do
+      tab <- get
+      case getSymbolInfo tab s name of
+        Just _ -> pure ()
+        Nothing -> throwError $ Undefined name
+    visitExpr s (FnCall e es) = visitExpr s e >> mapM_ (visitExpr s) es
+    visitExpr s (UnOpApp _ e) = visitExpr s e
+    visitExpr s (BinOpApp _ e1 e2) = visitExpr s e1 >> visitExpr s e2
+    visitExpr _ _ = pure ()
 
--- TODO: look for vars in exprs - these can only refer back, not forward
-visitItem :: (MonadState SymbolTable m, MonadError AnalysisError m)
-          => Item
-          -> m ()
-visitItem (StmtItem stmt) = visitStmt Nothing stmt
-visitItem (FnDefItem (FnDef name args ret body)) = do
-  fns <- gets fnSymbols
-  newFns <- M.alterF (\case
-    Nothing -> pure $ Just (FnEntry args ret, M.empty)
-    Just _ -> throwError $ AlreadyDefined name) name fns
-  modify (\s -> s { fnSymbols = newFns })
-  mapM_ (visitStmt (Just name)) body
-
-visitStmt :: (MonadState SymbolTable m, MonadError AnalysisError m)
-          => Maybe Ident
-          -> Stmt
-          -> m ()
-visitStmt scope = undefined
+fnTable :: Program -> Either AnalysisError FnTable
+fnTable = flip execStateT M.empty . mapM_ visitItem where
+  visitItem (FnDefItem (FnDef name args ret _)) = do
+    tab <- get
+    tab' <- M.alterF (\case
+      Nothing -> pure $ Just (args, ret)
+      Just _ -> throwError $ AlreadyDefined name) name tab
+    put tab'
+  visitItem _ = pure ()
