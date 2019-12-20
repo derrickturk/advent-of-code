7b5686139bf7826bb5566a34477c3e507a54c5ca
derrickturk <dwt@terminusdatascience.com>
2019-12-20 10:55:12 -0600

    Start on IR...

---
+++ 2019/prufrock/src/Prufrock/IR.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Prufrock.IR (
+    MonadIR(..)
+  , Src(..)
+  , Dst(..)
+  , FnCall(..)
+  , IRInstruction(..)
+) where
+
+import Data.Int (Int64)
+import qualified Data.Text as T
+
+{- TODO: where does AST -> IR code live?
+import Control.Monad.State.Strict
+
+class Monad m => MonadIR m where
+  gensym :: m T.Text
+-}
+
+data Src
+  = Const Int64 
+  | LValue Dst
+  deriving (Eq, Show)
+
+data Dst
+  = Global T.Text
+  | Stack Int64
+  deriving (Eq, Show)
+
+data FnCall
+  = CallGlobal T.Text
+  | CallPtr Src
+  deriving (Eq, Show)
+
+data IRInstruction
+  = Store Src Dst
+  | AddInto Src Src Dst
+  | MulInto Src Src Dst
+  | InputInto Dst
+  | OutputFrom Src
+  | Call FnCall [Src]
+  | Ret
+  deriving (Eq, Show)
