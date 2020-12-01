181b99a9e639ada42c0643e3e9a0427813b27f33
Derrick W. Turk <dwt@terminusdatascience.com>
2020-11-30 23:11:56 -0600

    oh yeah, these things have a part 2.

--- 2020/01/01.py
+++ 2020/01/01.py
@@ -6,6 +6,15 @@ def find2sum(xs):
             if i != j and x + y == 2020:
                 return x, y
 
+def find3sum(xs):
+    for (i, x) in enumerate(xs):
+        for (j, y) in enumerate(xs):
+            for (k, z) in enumerate(xs):
+                if i != j != k and x + y + z == 2020:
+                    return x, y, z
+
 inp = [int(l.strip()) for l in sys.stdin]
 x, y = find2sum(inp)
 print(x * y)
+x, y, z = find3sum(inp)
+print(x * y * z)
