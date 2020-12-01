eeeb3eccdd623e3632a95922b843ee9cc6fbd58f
Derrick W. Turk <dwt@terminusdatascience.com>
2020-11-30 23:18:50 -0600

    clean things up a bit

--- 2020/01/01.py
+++ 2020/01/01.py
@@ -1,20 +1,40 @@
 import sys
 
-def find2sum(xs):
+from typing import List, Optional, Tuple
+
+def find2sum(xs: List[int], target: int) -> Optional[Tuple[int, int]]:
     for (i, x) in enumerate(xs):
         for (j, y) in enumerate(xs):
-            if i != j and x + y == 2020:
+            if i != j and x + y == target:
                 return x, y
+    return None
 
-def find3sum(xs):
+def find3sum(xs: List[int], target: int) -> Optional[Tuple[int, int, int]]:
     for (i, x) in enumerate(xs):
         for (j, y) in enumerate(xs):
             for (k, z) in enumerate(xs):
                 if i != j != k and x + y + z == 2020:
                     return x, y, z
+    return None
 
-inp = [int(l.strip()) for l in sys.stdin]
-x, y = find2sum(inp)
-print(x * y)
-x, y, z = find3sum(inp)
-print(x * y * z)
+def main() -> int:
+    inp = [int(l.strip()) for l in sys.stdin]
+
+    res2 = find2sum(inp, 2020)
+    if res2 is None:
+        print('no 2-element solution')
+    else:
+        x, y = res2
+        print(x * y)
+
+    res3 = find3sum(inp, 2020)
+    if res3 is None:
+        print('no 3-element solution')
+    else:
+        x, y, z = res3
+        print(x * y * z)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
