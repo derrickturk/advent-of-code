ffca69f53ed6bffe8c914d4d0c4f1b087766a02c
Derrick W. Turk <dwt@terminusdatascience.com>
2020-12-10 11:29:41 -0600

    I hate when they force me into the state monad

--- 2020/10/10.hs
+++ 2020/10/10.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE TypeApplications #-}
 
+import Data.Array.Unboxed
+import Control.Monad.State
 import Data.List (sort)
 import Text.Read (readMaybe)
+import qualified Data.Map.Strict as M
 
 -- they define this with an implicit starting 0
 diff :: Num a => [a] -> [a]
@@ -13,13 +16,34 @@ adapterChain xs = sort $ (3 + maximum xs):xs
 count :: (a -> Bool) -> [a] -> Int
 count p = length . filter p
 
-chains :: (Ord a, Num a) => a -> a -> [a] -> Integer
-chains last target []
-  | last == target = 1
-  | otherwise = 0
-chains last target (x:xs)
-  | x - last > 3 = 0
-  | otherwise = chains x target xs + chains last target xs
+chains :: Int
+       -> Int
+       -> Int
+       -> UArray Int Int
+       -> State (M.Map (Int, Int) Integer) Integer
+chains target last ix xs = do
+  val <- gets $ M.lookup (last, ix)
+  case val of
+    Just count -> pure count
+    Nothing -> do
+      let (_, ixLast) = bounds xs
+      count <- if ix > ixLast
+                 then if target == last
+                   then pure 1
+                   else pure 0
+                 else let x = xs ! ix
+                       in if x - last > 3
+                         then pure 0
+                         else (+) <$> chains target x (ix + 1) xs
+                                  <*> chains target last (ix + 1) xs
+      modify (M.insert (last, ix) count)
+      pure count
+
+fullChains :: [Int] -> Integer
+fullChains [] = 0
+fullChains xs = let lastIx = length xs - 1
+                    xs' = listArray (0, lastIx) xs
+                 in evalState (chains (xs' ! lastIx) 0 0 xs') M.empty
 
 main :: IO ()
 main = do
@@ -31,5 +55,5 @@ main = do
           ones = count (== 1) diffs
           threes = count (== 3) diffs
       print $ ones * threes
-      print $ chains 0 (last adapters) adapters
+      print $ fullChains adapters
     _ -> putStrLn "invalid input"
