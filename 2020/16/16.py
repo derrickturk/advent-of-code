6d2d8667b985f1929353f03f2b21b30e0372c76d
Derrick W. Turk <dwt@terminusdatascience.com>
2020-12-16 10:00:07 -0600

    backtracking is correct but too slow

--- 2020/16/16.py
+++ 2020/16/16.py
@@ -1,11 +1,19 @@
 import sys
 
-from typing import Dict, List, NamedTuple, TextIO, Tuple
+from typing import Dict, List, NamedTuple, Optional, TextIO, Tuple
+
+RuleClause = Tuple[int, int] # e.g. 25-35
+Rule = List[RuleClause] # e.g. 25-35 or 47-90
+RuleMap = Dict[str, Rule] # e.g. xxx: 25-35 or 47-90
+
+Ticket = List[int]
+
+RuleAssignment = List[str]
 
 class Data(NamedTuple):
-    rules: Dict[str, List[Tuple[int, int]]]
-    mine: List[int]
-    others: List[List[int]]
+    rules: RuleMap
+    mine: Ticket
+    others: List[Ticket]
 
 def parse_input(stream: TextIO) -> Data:
     rules = dict()
@@ -39,14 +47,14 @@ def parse_input(stream: TextIO) -> Data:
 
     return Data(rules, mine, others)
 
-def valid(rules: Dict[str, List[Tuple[int, int]]], rule: str, val: int) -> bool:
+def valid(rules: RuleMap, rule: str, val: int) -> bool:
     clauses = rules[rule]
     for (low, high) in clauses:
         if low <= val and val <= high:
             return True
     return False
 
-def any_valid(rules: Dict[str, List[Tuple[int, int]]], val: int) -> bool:
+def any_valid(rules: RuleMap, val: int) -> bool:
     return any(low <= val <= high
       for clauses in rules.values()
       for (low, high) in clauses)
@@ -59,9 +67,62 @@ def error_rate(data: Data) -> int:
                 rate += val
     return rate
 
+def valid_tickets(data: Data) -> List[Ticket]:
+    result = list()
+    for t in data.others:
+        valid = True
+        for val in t:
+            if not any_valid(data.rules, val):
+                valid = False
+                break
+        if valid:
+            result.append(t)
+    return result
+
+def solve_mapping(data: Data) -> RuleAssignment:
+    valid_tix = valid_tickets(data)
+    res = solve_sub_mapping(data.rules, valid_tix, [''] * len(data.mine), 0)
+    if res is None:
+        raise ValueError('no solution!')
+    return res
+
+def solve_sub_mapping(rules: RuleMap, tix: List[Ticket],
+        mapping: RuleAssignment, start: int) -> Optional[RuleAssignment]:
+    if start == len(mapping):
+        return mapping
+
+    locked = set(mapping[:start])
+    possible = set(k for k in rules if k not in locked)
+    for p in possible:
+        try_mapping = list(mapping)
+        try_mapping[start] = p
+
+        discredited = False
+        for t in tix:
+            if not valid(rules, p, t[start]):
+                discredited = True
+                break
+        if discredited:
+            continue
+
+        solved = solve_sub_mapping(rules, tix, try_mapping, start + 1)
+        if solved is not None:
+            return solved
+
+    return None
+
 def main() -> int:
     data = parse_input(sys.stdin)
+
     print(error_rate(data))
+
+    assignment = solve_mapping(data)
+
+    prod = 1
+    for i, rule in enumerate(assignment):
+        if rule.startswith('departure'):
+            prod *= data.mine[i]
+    print(prod)
 
     return 0
 
