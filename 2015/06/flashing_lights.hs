57a6d7ea9f7e733debb2ede7a0e57eeffe190a26
Derrick W. Turk <dwt@terminusdatascience.com>
2021-11-18 13:51:41 -0600

    haskell, the choice of (slow) fancymen

--- 2015/06/flashing_lights.hs
+++ 2015/06/flashing_lights.hs
@@ -1,31 +1,58 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.Set as S
+import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Data.List (foldl')
 import Data.Char (isSpace)
 import Text.Read (readMaybe)
 
-newtype Grid = Grid { getGrid :: S.Set (Int, Int) }
+class Griddy a where
+  unlit :: a
+  turnOn :: (Int, Int) -> a -> a
+  turnOff :: (Int, Int) -> a -> a
+  toggle :: (Int, Int) -> a -> a
+
+newtype BasicGrid = BasicGrid { getBasicGrid :: S.Set (Int, Int) }
   deriving (Eq, Show)
 
-unlit :: Grid
-unlit = Grid S.empty
+instance Griddy BasicGrid where
+  unlit = BasicGrid S.empty
 
-turnOn :: (Int, Int) -> Grid -> Grid
-turnOn c (Grid s) = Grid $ S.insert c s
+  turnOn c (BasicGrid s) = BasicGrid $ S.insert c s
 
-turnOff :: (Int, Int) -> Grid -> Grid
-turnOff c (Grid s) = Grid $ S.delete c s
+  turnOff c (BasicGrid s) = BasicGrid $ S.delete c s
 
-toggle :: (Int, Int) -> Grid -> Grid
-toggle c g@(Grid s) = if S.member c s
-  then turnOff c g
-  else turnOn c g
+  toggle c g@(BasicGrid s) = if S.member c s
+    then turnOff c g
+    else turnOn c g
 
-countLit :: Grid -> Int
-countLit = S.size . getGrid
+countLit :: BasicGrid -> Int
+countLit = S.size . getBasicGrid
+
+newtype FancyGrid = FancyGrid { getFancyGrid :: M.Map (Int, Int) Int }
+  deriving (Eq, Show)
+
+instance Griddy FancyGrid where
+  unlit = FancyGrid M.empty
+
+  turnOn c (FancyGrid m) = FancyGrid $ M.alter f c m where
+    f Nothing = Just 1
+    f (Just n) = Just (n + 1)
+
+  turnOff c (FancyGrid m) = FancyGrid $ M.alter f c m where
+    f Nothing = Nothing
+    f (Just n)
+      | n > 1 = Just (n - 1)
+      | otherwise = Nothing
+
+  toggle c (FancyGrid m) = FancyGrid $ M.alter f c m where
+    f Nothing = Just 2
+    f (Just n) = Just (n + 2)
+
+countLumens :: FancyGrid -> Integer
+countLumens = sum . fmap fromIntegral . M.elems . getFancyGrid
 
 data Instruction
   = TurnOn (Int, Int) (Int, Int)
@@ -36,16 +63,16 @@ data Instruction
 range :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
 range (x0, y0) (x1, y1) = [(x, y) | x <- [x0..x1], y <- [y0..y1]]
 
-apply :: Instruction -> Grid -> Grid
+apply :: Griddy g => Instruction -> g -> g
 apply (TurnOn from to) = foldGridOp turnOn (range from to)
 apply (TurnOff from to) = foldGridOp turnOff (range from to)
 apply (Toggle from to) = foldGridOp toggle (range from to)
 
-foldGridOp :: ((Int, Int) -> Grid -> Grid) -> [(Int, Int)] -> Grid -> Grid
+foldGridOp :: Griddy g => ((Int, Int) -> g -> g) -> [(Int, Int)] -> g -> g
 foldGridOp op coords init = foldl' step init coords where
   step g c = op c g
 
-applyAll :: [Instruction] -> Grid -> Grid
+applyAll :: Griddy g => [Instruction] -> g -> g
 applyAll instrs init = foldl' (flip apply) init instrs
 
 parseInstruction :: T.Text -> Maybe Instruction
@@ -63,3 +90,4 @@ main :: IO ()
 main = do
   Just instrs <- traverse parseInstruction . T.lines <$> TIO.getContents
   print $ countLit $ applyAll instrs unlit
+  print $ countLumens $ applyAll instrs unlit
