e6be38b6d461a5bef5a8206305d9492d41e7b046
Derrick W. Turk <dwt@terminusdatascience.com>
2021-11-19 12:00:43 -0600

    monadic deer

--- 2015/14/d33r.hs
+++ 2015/14/d33r.hs
@@ -2,8 +2,13 @@
 
 import Data.Char (isSpace)
 import Text.Read (readMaybe)
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+import Data.Foldable (traverse_)
+import Control.Monad.State (State, evalState, gets, modify)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Data.Map.Strict as M
 
 type Deer = (T.Text, Int, Int, Int)
 
@@ -39,8 +44,26 @@ deerPos time (_, speed, duration, rest) =
       partialCycle' = partialCycle `min` duration
    in fullCycles * perCycle + partialCycle' * speed
 
+type DeerContest = M.Map T.Text Int
+
+deerRace :: Int -> [Deer] -> (T.Text, Int)
+deerRace time deer = evalState deerRace' initial where
+  deerRace' = do
+    traverse_ (deerStep deer) [1..time]
+    gets (maximumBy (comparing snd) . M.toList)
+  initial = M.fromList $ (\(name, _, _, _) -> (name, 0)) <$> deer
+
+deerStep :: [Deer] -> Int -> State DeerContest ()
+deerStep deer time = do
+  let positions = deerPos time <$> deer
+      positions' = zip ((\(name, _, _, _) -> name) <$> deer) positions
+      fastest = maximum positions
+      winners = filter ((== fastest) . snd) positions'
+  traverse_ (\(name, _) -> modify (M.adjust (+ 1) name)) winners
+
 main :: IO ()
 main = do
   Just deer <- traverse parseDeer . T.lines <$> TIO.getContents
   let positions = deerPos 2503 <$> deer
   print $ maximum positions
+  print $ deerRace 2503 deer
