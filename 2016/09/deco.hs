8fef95928e5b7bde5a57555701fc717059aa97a7
Derrick W. Turk <dwt@terminusdatascience.com>
2021-11-25 21:14:00 -0600

    double meh

--- 2016/09/deco.hs
+++ 2016/09/deco.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Monoid (Sum(..))
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
@@ -25,8 +26,26 @@ repeater = do
 code :: Parser T.Text
 code = mconcat <$> many (repeater <|> letters)
 
+repeaterLen :: Parser (Sum Int)
+repeaterLen = do
+  _ <- "("
+  n <- unsignedIntNum
+  _ <- "x"
+  m <- unsignedIntNum
+  _ <- ")"
+  what <- nChars n
+  case runParser (only codeLen) what of
+    Just (Sum x, _) -> pure (Sum (m * x))
+    Nothing -> pure (Sum 0)
+
+codeLen :: Parser (Sum Int)
+codeLen = mconcat <$> many (repeaterLen <|> (Sum . T.length) <$> letters)
+
 main :: IO ()
 main = do
-  Just codes <- parseStdin $ many $ lexeme code
+  input <- TIO.getContents
+  let Just codes = parse (only $ many $ lexeme code) input
+      Just lens = parse (only $ many $ lexeme codeLen) input
   mapM_ TIO.putStrLn codes
   print $ sum $ T.length <$> codes
+  print $ sum lens
