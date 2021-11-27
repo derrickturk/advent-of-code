895e2a7681a7fbacf67355f617e76d60194a2987
Derrick W. Turk <dwt@terminusdatascience.com>
2021-11-26 19:42:58 -0600

    Revert "WIP"
    This reverts commit df572ff257811bd57f74be4911e97ad963158738.

--- 2016/11/notorious.hs
+++ 2016/11/notorious.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE DataKinds, OverloadedStrings, StandaloneDeriving, TypeFamilies #-}
-{-# LANGUAGE TypeFamilyDependencies, FlexibleContexts, UndecidableInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- the notorious RTG
 
@@ -7,8 +6,6 @@ import qualified Data.Text as T
 import qualified Data.Set as S
 import Control.Monad (guard)
 import Data.List (foldl')
-import Data.Kind (Type)
-import Prelude hiding (null)
 
 import FemtoParsec
 import Dijkstra
@@ -25,41 +22,21 @@ data Item
   | RTG T.Text
   deriving (Show, Eq, Ord)
 
-data FastFloor
-  = FastFloor { pairs :: Int
-              , loneRTGs :: Int
-              , loneChips :: [Floor] -- floor of corresponding RTG
-              } deriving (Show, Eq, Ord)
-
-data ReprKind
-  = Normie
-  | Fast
-
-class FloorRepr (a :: ReprKind) where
-  type Repr a = (r :: Type) | r -> a
-  validItemMoves :: (Floor, Repr a) -> (Floor, Repr a) -> [(Repr a, Repr a)]
-  fried :: Repr a -> Bool
-  null :: Repr a -> Bool
-
-data State (a :: ReprKind)
+data State
   = State { elevatorFloor :: Floor
-          , firstFloor :: Repr a
-          , secondFloor :: Repr a
-          , thirdFloor :: Repr a
-          , fourthFloor :: Repr a
-          }
+          , firstFloor :: S.Set Item
+          , secondFloor :: S.Set Item
+          , thirdFloor :: S.Set Item
+          , fourthFloor :: S.Set Item
+          } deriving (Show, Eq, Ord)
 
-deriving instance Show (Repr a) => Show (State a)
-deriving instance Eq (Repr a) => Eq (State a)
-deriving instance Ord (Repr a) => Ord (State a)
-
-floorItems :: Floor -> State a -> Repr a
+floorItems :: Floor -> State -> S.Set Item
 floorItems One = firstFloor
 floorItems Two = secondFloor
 floorItems Three = thirdFloor
 floorItems Four = fourthFloor
 
-withFloor :: Floor -> Repr a -> State a -> State a
+withFloor :: Floor -> S.Set Item -> State -> State
 withFloor One contents s = s { firstFloor = contents }
 withFloor Two contents s = s { secondFloor = contents }
 withFloor Three contents s = s { thirdFloor = contents }
@@ -70,6 +47,16 @@ validElevatorMoves One = [Two]
 validElevatorMoves Two = [One, Three]
 validElevatorMoves Three = [Two, Four]
 validElevatorMoves Four = [Three]
+
+friedChip :: S.Set Item -> Bool
+friedChip s = any bad xs where
+  xs = S.toList s
+
+  bad (RTG _) = False
+  bad (Chip lbl) = any isRTG xs && not (S.member (RTG lbl) s)
+
+  isRTG (RTG _) = True
+  isRTG _ = False
 
 leaveOneOut :: [a] -> [(a, [a])]
 leaveOneOut [] = []
@@ -83,49 +70,49 @@ choose xs n = do
   (group, rest') <- choose rest (n - 1)
   pure (x:group, rest')
 
-instance FloorRepr 'Normie where
-  type Repr 'Normie = S.Set Item
+-- moves of one or two items which Preserve Balance
+-- this is where the Fun begins
+validItemMoves :: S.Set Item -> S.Set Item -> [(S.Set Item, S.Set Item)]
+validItemMoves from to = moveTwo <> moveOne where
+  moveOne = do
+    (chosen, rest) <- leaveOneOut $ S.toList from
+    let from' = S.fromList rest
+        to' = S.insert chosen to
+    guard (not (friedChip from') && not (friedChip to'))
+    pure (from', to')
+  moveTwo = do
+    (chosen, rest) <- choose (S.toList from) 2
+    let from' = S.fromList rest
+        to' = foldl' (flip S.insert) to chosen
+    guard (not (friedChip from') && not (friedChip to'))
+    pure (from', to')
 
-  -- moves of one or two items which Preserve Balance
-  -- this is where the Fun begins
-  validItemMoves (_, from) (_, to) = moveTwo <> moveOne where
-    moveOne = do
-      (chosen, rest) <- leaveOneOut $ S.toList from
-      let from' = S.fromList rest
-          to' = S.insert chosen to
-      guard (not (fried from') && not (fried to'))
-      pure (from', to')
-    moveTwo = do
-      (chosen, rest) <- choose (S.toList from) 2
-      let from' = S.fromList rest
-          to' = foldl' (flip S.insert) to chosen
-      guard (not (fried from') && not (fried to'))
-      pure (from', to')
-
-  fried s = any bad xs where
-    xs = S.toList s
-    bad (RTG _) = False
-    bad (Chip lbl) = any isRTG xs && not (S.member (RTG lbl) s)
-    isRTG (RTG _) = True
-    isRTG _ = False
-
-  null = S.null
-
-validMoves :: FloorRepr a => State a -> [State a]
+validMoves :: State -> [State]
 validMoves s = do
   let oldFloor = elevatorFloor s
   newFloor <- validElevatorMoves oldFloor
-  (from', to') <- validItemMoves
-    (oldFloor, (floorItems oldFloor s)) (newFloor, (floorItems newFloor s))
+  (from', to') <- validItemMoves (floorItems oldFloor s) (floorItems newFloor s)
   let s' = s { elevatorFloor = newFloor }
       s'' = withFloor oldFloor from' s'
       s''' = withFloor newFloor to' s''
   pure s'''
 
 -- all items on fourth floor
-won :: FloorRepr a => State a -> Bool
-won s = elevatorFloor s == Four && null (firstFloor s)
-  && null (secondFloor s) && null (thirdFloor s)
+won :: State -> Bool
+won s = elevatorFloor s == Four && S.null (firstFloor s)
+  && S.null (secondFloor s) && S.null (thirdFloor s)
+
+{-
+cheapestWin :: State -> Int
+cheapestWin = cheapestWin' . qStart where
+  cheapestWin' q = case shift q of
+    Nothing -> maxBound
+    Just (cost, s, _) | won s -> cost
+    Just (cost, s, rest) ->
+      let steps = [(1 + cost, s') | s' <- validMoves s]
+          q' = foldl (flip enqueueOrUpdate) rest steps
+       in cheapestWin' q'
+-}
 
 item :: Parser Item
 item =  RTG <$> ("a " *> letters <* " generator")
@@ -137,7 +124,7 @@ items =  S.fromList <$> sepBy sep item
   where
     sep = ", and " <|> " and " <|> ", "
 
-world :: Parser (State 'Normie)
+world :: Parser State
 world = do
   _ <- "The first floor contains "
   f1 <- items
