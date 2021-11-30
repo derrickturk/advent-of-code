6336a988c2a62adaac7be860ac1a0f43ddcb9768
Derrick W. Turk <dwt@terminusdatascience.com>
2021-11-29 23:12:08 -0600

    hmm

--- 2016/21/dumpster.hs
+++ 2016/21/dumpster.hs
@@ -1,11 +1,12 @@
-{-# LANGUAGE RankNTypes, OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes, OverloadedStrings #-}
 
 import Control.Monad.ST
 import Data.Array.ST
 import Data.Array.IArray (elems)
-import Control.Monad (foldM)
+import Control.Monad (foldM, forM_)
 import System.Environment (getArgs)
 import Data.Char (isAlpha)
+import Data.List (find)
 import Prelude hiding (min, max)
 
 import FemtoParsec
@@ -18,7 +19,18 @@ data Instr
   | RotLtr Char
   | RevSpan Int Int
   | Move Int Int
+  | UnRotLtr Char -- the FORBIDDEN ANTI-INSTRUCTION
   deriving Show
+
+undo :: Instr -> Instr
+undo (SwapPos x y) = (SwapPos y x)
+undo (SwapLtr c d) = (SwapLtr d c)
+undo (RotL n) = RotR n
+undo (RotR n) = RotL n
+undo (RotLtr c) = UnRotLtr c
+undo (RevSpan i j) = RevSpan i j
+undo (Move i j) = Move j i
+undo (UnRotLtr c) = RotLtr c
 
 initialize :: String -> ST s (STUArray s Int Char)
 initialize pass = newListArray (0, length pass - 1) pass
@@ -36,23 +48,61 @@ run a (SwapLtr c d) = mapArray swap a where
     | x == d = c
     | otherwise = x
 run a (RotL n) = do
-  (min, max) <- getBounds a
-  let len = max - min + 1
-  newElems <- traverse
-    (\i -> readArray a (min + ((i - min + n) `mod` len))) [min..max]
-  newListArray (min, max) newElems
+  (0, end) <- getBounds a
+  let len = end + 1
+  mapIndices (0, end) (\i -> (i + n) `mod` len) a
 run a (RotR n) = do
-  (min, max) <- getBounds a
-  let len = max - min + 1
-  newElems <- traverse
-    (\i -> readArray a (min + ((i - min - n) `mod` len))) [min..max]
-  newListArray (min, max) newElems
-run a _ = pure a
+  (0, end) <- getBounds a
+  let len = end + 1
+  mapIndices (0, end) (\i -> (i - n) `mod` len) a
+run a (RotLtr c) = do
+  which <- find ((== c) . snd) <$> getAssocs a
+  case which of
+    Just (i, _) -> run a (RotR $ 1 + (if i >= 4 then 1 else 0) + i)
+    Nothing -> pure a
+run a (RevSpan from to) = do
+  (0, end) <- getBounds a
+  let remap i = if i < from || i > to
+                  then i
+                  else to - (i - from)
+  mapIndices (0, end) remap a
+run a (Move i j) = do
+  (0, end) <- getBounds a
+  val <- readArray a i
+  -- "delete" at i
+  forM_ [i..end - 1] $ \k -> do
+    x <- readArray a (k + 1)
+    writeArray a k x
+  -- "insert" at j
+  forM_ [end, end - 1 .. j + 1] $ \k -> do
+    x <- readArray a (k - 1)
+    writeArray a k x
+  writeArray a j val
+  pure a
+run a (UnRotLtr c) = do
+  a' <- run a (RotL 1)
+  go 0 a'
+  where
+    unrotix i = if i < 6
+                  then i - 1
+                  else i - 2
+    go n arr = do
+      x <- readArray arr (unrotix n)
+      if x == c
+        then pure arr
+        else do
+          arr' <- run arr (RotL 1)
+          go (n + 1) arr'
 
 scramble :: String -> [Instr] -> String
 scramble pass instrs = elems $ runSTUArray $ do
   a <- initialize pass
   foldM run a instrs
+
+unscramble :: String -> [Instr] -> String
+unscramble pass instrs = elems $ runSTUArray $ do
+  a <- initialize pass
+  foldM (\arr i -> run arr (undo i)) a $ reverse instrs
 
 instr :: Parser Instr
 instr = swapPos <|> swapLtr <|> rotL <|> rotR <|> rotLtr <|> revSpan <|> move
@@ -74,5 +124,5 @@ main :: IO ()
 main = do
   [pass] <- getArgs
   Just instrs <- parseStdin $ many $ lexeme instr
-  print instrs
   print $ scramble pass instrs
+  print $ unscramble pass instrs
