4f06c4f18ac750dfffc6222096818d31c4d81c93
Derrick W. Turk <dwt@terminusdatascience.com>
2021-12-01 22:53:36 -0600

    Pt1

---
+++ 2017/17/spinnaz.hs
@@ -0,0 +1,18 @@
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
+
+import Circ
+
+spinStep :: Int -> Int -> Circ Int -> Circ Int
+spinStep i n c = insert i (rotateNR n c)
+
+spinGame :: Int -> [Circ Int]
+spinGame n = snd <$> iterate f (1, singleton 0) where
+  f (i, c) = (i + 1, spinStep i n c)
+
+main :: IO ()
+main = do
+  [n] <- getArgs
+  let Just n' = readMaybe n
+  print $ focus $ rotateR $ spinGame 3 !! 2017
+  print $ focus $ rotateR $ spinGame n' !! 2017
