49962b8f1b981247ea355d177a58025581e2b4ac
Derrick W. Turk <dwt@terminusdatascience.com>
2021-12-07 21:11:50 -0600

    fun with constraint propagation

--- 2018/16/mysterymachine.hs
+++ 2018/16/mysterymachine.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad (foldM)
 import Data.Bits
+import Data.List (foldl')
 import Prelude hiding (read)
 
 import FemtoParsec
@@ -123,6 +125,9 @@ step cpu (Instr Eqir a b c) = testIR (==) a b c cpu
 step cpu (Instr Eqri a b c) = testRI (==) a b c cpu
 step cpu (Instr Eqrr a b c) = testRR (==) a b c cpu
 
+run :: Cpu -> [Instr] -> Either Error Cpu
+run cpu is = foldM step cpu is 
+
 exampleP :: Parser Example
 exampleP = do
   _ <- "Before: ["
@@ -139,11 +144,49 @@ progP = many $ do
   [o, a, b, c] <- lexeme $ sepBy " " intNum
   pure (o, a, b, c)
 
-checkEx :: Example -> Int
-checkEx (Example bs as (_, a, b, c)) = length $ filter f allOpcodes where
+checkEx :: Example -> (Int, [Opcode])
+checkEx (Example bs as (n, a, b, c)) = (n, filter f allOpcodes) where
   f op = step bs (Instr op a b c) == Right as
+
+possibleOpcodes :: [Example] -> [(Int, [Opcode])]
+possibleOpcodes = foldl' refine allPoss where
+  allPoss :: [(Int, [Opcode])]
+  allPoss = zip [0..15] $ repeat allOpcodes
+  refine ps ex = let (i, ops) = checkEx ex
+                  in alter i (whereOnly ops) ps
+  whereOnly these those = filter (`elem` these) those
+  alter _ _ [] = []
+  alter x f ((y, v):rest)
+    | y == x = (x, f v):rest
+    | otherwise = (y, v):alter x f rest
+
+propagate :: [(Int, [Opcode])] -> Maybe [(Int, Opcode)]
+propagate opmap
+  | all ((== 1) . length . snd) opmap =
+      Just $ (\(n, os) -> (n, head os)) <$> opmap
+  | otherwise = if opmap' == opmap
+                  then Nothing
+                  else propagate opmap'
+  where
+    opmap' = foldl' refine opmap (filter ((== 1) . length . snd) opmap)
+    refine :: [(Int, [Opcode])] -> (Int, [Opcode]) -> [(Int, [Opcode])]
+    refine om (n, [op]) = fmap (prop1 n op) om
+    refine _ _ = error "internal goof"
+    prop1 n op (m, os)
+      | n == m = (m, os)
+      | otherwise = (m, filter (/= op) os)
+
+mapOpcodes :: [Example] -> Maybe [(Int, Opcode)]
+mapOpcodes = propagate . possibleOpcodes
+
+decode :: [(Int, Opcode)] -> (Int, Int, Int, Int) -> Maybe Instr
+decode m (o, a, b, c) = Instr <$> lookup o m <*> pure a <*> pure b <*> pure c
 
 main :: IO ()
 main = do
-  Just (ex, _) <- parseStdin $ (,) <$> many (lexeme exampleP) <*> progP
-  print $ length $ filter (\e -> checkEx e >= 3) ex
+  Just (ex, prog) <- parseStdin $ (,) <$> many (lexeme exampleP) <*> progP
+  print $ length $ filter (\e -> length (checkEx e) >= 3) ex
+  let Just opmap = mapOpcodes ex
+      Just prog' = traverse (decode opmap) prog
+      Right cpu = run (Cpu (0, 0, 0, 0)) prog'
+  print cpu
