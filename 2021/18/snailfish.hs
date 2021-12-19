2192f58f788de19a8d5f03eddd38e2c96afd3c6e
Derrick W. Turk <dwt@terminusdatascience.com>
2021-12-18 18:13:20 -0600

    cookingwithzippersnow

--- 2021/18/snailfish.hs
+++ 2021/18/snailfish.hs
@@ -1,74 +1,107 @@
 {-# LANGUAGE OverloadedStrings, TypeApplications #-}
 
-import Data.Maybe (fromJust)
+import Prelude hiding (Left, Right)
 
 import FemtoParsec
 
-data SnailfishElem
-  = Val Int
-  | Nest Snailfish
-  deriving Show
+import Debug.Trace
 
+-- this is technically wrong but the other way made life miserable
 data Snailfish
-  = Pair SnailfishElem SnailfishElem
+  = Val Int
+  | Pair Snailfish Snailfish
   deriving Show
 
-data Step
-  = L
-  | R
+data Path
+  = Top
+  | Left Path Snailfish
+  | Right Path Snailfish
   deriving Show
 
-type Cursor = [Step]
-type Zipper = (Snailfish, Cursor)
+newtype Loc = Loc { unLoc :: (Snailfish, Path) }
+  deriving Show
 
-{-
+depth :: Path -> Int
+depth Top = 0
+depth (Left p _) = 1 + depth p
+depth (Right p _) = 1 + depth p
 
-readC :: Cursor -> Snailfish -> Maybe SnailfishElem
-readC [L] (Pair lhs _) = Just lhs
-readC [R] (Pair _ rhs) = Just rhs
-readC (L:rest) (Pair (Nest lhs) _) = readC rest lhs
-readC (R:rest) (Pair _ (Nest rhs)) = readC rest rhs
-readC _ _ = Nothing
+alter :: Loc -> Snailfish -> Loc
+alter (Loc (_, p)) s = Loc (s, p)
 
-modifyC :: Cursor
-        -> (SnailfishElem -> SnailfishElem)
-        -> Snailfish
-        -> Maybe Snailfish
-modifyC [L] f (Pair lhs rhs) = Just $ Pair (f lhs) rhs
-modifyC [R] f (Pair lhs rhs) = Just $ Pair lhs (f rhs)
-modifyC (L:rest) f (Pair (Nest lhs) rhs) =
-  Pair <$> (Nest <$> modifyC rest f lhs) <*> pure rhs
-modifyC (R:rest) f (Pair lhs (Nest rhs)) =
-  Pair <$> pure lhs <*> (Nest <$> modifyC rest f rhs)
-modifyC _ _ _ = Nothing
+modify :: Loc -> (Snailfish -> Snailfish) -> Loc
+modify (Loc (s, p)) f = Loc (f s, p)
 
-writeC :: Cursor -> SnailfishElem -> Snailfish -> Maybe Snailfish
-writeC cur e s = modifyC cur (const e) s
+parent :: Loc -> Maybe Loc
+parent (Loc (_, Top)) = Nothing
+parent (Loc (s, Left path right)) = Just $ Loc (Pair s right, path)
+parent (Loc (s, Right path left)) = Just $ Loc (Pair left s, path)
 
-stepLeft :: Cursor -> Maybe Cursor
-stepLeft [] = Nothing
-stepLeft [R] = Just [L]
-stepLeft [L] = Nothing
-stepLeft (L:rest) = (L:) <$> stepLeft rest
-stepLeft (R:rest) = (R:) <$> stepLeft rest <|> Just (L:(R <$ rest))
+leftSibling :: Loc -> Maybe Loc
+leftSibling (Loc (_, Top)) = Nothing
+leftSibling (Loc (_, Left _ _)) = Nothing
+leftSibling (Loc (s, Right path left)) = Just $ Loc (left, Left path s)
 
-stepRight :: Cursor -> Maybe Cursor
-stepRight [] = Nothing
-stepRight [L] = Just [R]
-stepRight [R] = Nothing
-stepRight (R:rest) = (R:) <$> stepRight rest
-stepRight (L:rest) = (L:) <$> stepRight rest <|> Just (R:(L <$ rest))
+rightSibling :: Loc -> Maybe Loc
+rightSibling (Loc (_, Top)) = Nothing
+rightSibling (Loc (_, Right _ _)) = Nothing
+rightSibling (Loc (s, Left path right)) = Just $ Loc (right, Left path s)
 
-lefties :: Cursor -> [Cursor]
-lefties c = case stepLeft c of
-  Just c' -> c':lefties c'
-  Nothing -> []
+leftChild :: Loc -> Maybe Loc
+leftChild (Loc (Val _, _)) = Nothing
+leftChild (Loc (Pair l r, path)) = Just $ Loc (l, Left path r)
 
-righties :: Cursor -> [Cursor]
-righties c = case stepRight c of
-  Just c' -> c':righties c'
-  Nothing -> []
--}
+rightChild :: Loc -> Maybe Loc
+rightChild (Loc (Val _, _)) = Nothing
+rightChild (Loc (Pair l r, path)) = Just $ Loc (r, Right path l)
+
+root :: Snailfish -> Loc
+root s = Loc (s, Top)
+
+leftNum :: Loc -> Maybe Loc
+leftNum (Loc (_, Top)) = Nothing
+leftNum (Loc (s, Right path left@(Val _))) = Just $ Loc (left, Left path s)
+leftNum l = parent l >>= go where
+  go l' = (leftSibling l' >>= leftmost' p) <|> (parent l' >>= go)
+  p (Loc (Val _, _)) = True
+  p _ = False
+
+rightNum :: Loc -> Maybe Loc
+rightNum (Loc (_, Top)) = Nothing
+rightNum (Loc (s, Left path right@(Val _))) = Just $ Loc (right, Right path s)
+rightNum l = parent l >>= go where
+  go l' = (rightSibling l' >>= rightmost' p) <|> (parent l' >>= go)
+  p (Loc (Val _, _)) = True
+  p _ = False
+
+leftmost :: (Loc -> Bool) -> Snailfish -> Maybe Loc
+leftmost predicate s = leftmost' predicate (root s)
+
+leftmost' :: (Loc -> Bool) -> Loc -> Maybe Loc
+leftmost' predicate loc =  goLeft predicate loc
+                       <|> rightHere predicate loc
+                       <|> goRight predicate loc
+  where
+    goLeft p l = leftChild l >>= leftmost' p
+    rightHere p l = if p l then Just l else Nothing
+    goRight p l = rightChild l >>= leftmost' p
+
+rightmost :: (Loc -> Bool) -> Snailfish -> Maybe Loc
+rightmost predicate s = rightmost' predicate (root s)
+
+rightmost' :: (Loc -> Bool) -> Loc -> Maybe Loc
+rightmost' predicate loc =  goRight predicate loc
+                        <|> rightHere predicate loc
+                        <|> goLeft predicate loc
+  where
+    goRight p l = rightChild l >>= rightmost' p
+    rightHere p l = if p l then Just l else Nothing
+    goLeft p l = leftChild l >>= rightmost' p
+
+leftmost4 :: Snailfish -> Maybe Loc
+leftmost4 = leftmost p where
+  p (Loc (Pair _ _, path)) = depth path >= 4
+  p _ = False
 
 {-
 add :: Snailfish -> Snailfish -> Snailfish
@@ -133,9 +166,11 @@ snailfish = do
   pure $ Pair lhs rhs
   where
     snailElem =  Val <$> unsignedIntNum
-             <|> Nest <$> snailfish
+             <|> snailfish
 
 main :: IO ()
 main = do
   Just fishes <- parseStdin (some $ lexeme snailfish)
-  print fishes
+  print $ leftmost4 $ head fishes
+  print $ (leftmost4 $ head fishes) >>= leftNum
+  print $ (leftmost4 $ head fishes) >>= rightNum
