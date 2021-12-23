e4e908fae2738d866a390aa1e62ec44afa1f7510
Derrick W. Turk <dwt@terminusdatascience.com>
2021-12-23 15:20:22 -0600

    blaaaaaghghghugyuhghghghg

--- 2021/23/amphipod.hs
+++ 2021/23/amphipod.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad (guard)
-import Data.Maybe (isJust)
+import Data.List (find)
+import Data.Maybe (isJust, maybeToList)
 import Data.Array
 
 import AStar
@@ -12,7 +13,7 @@ data Bug
   = A | B | C | D
   deriving (Eq, Show, Ord)
 
-type Room = (Maybe Bug, Maybe Bug)
+type Room = Array Int (Maybe Bug)
 type Rooms = Array Int Room
 type Hallway = Array Int (Maybe Bug)
 type World = (Rooms, Hallway)
@@ -69,125 +70,80 @@ clearBetween1 :: Hallway -> Int -> Int -> Bool
 clearBetween1 hall i j = all ((== Nothing) . (hall !)) $
   if i < j then [i+1..j] else [j..i-1]
 
+topOccupied :: Room -> Maybe (Int, Bug)
+topOccupied rm = case find (isJust . snd) $ assocs rm of
+  Just (i, Just bug) -> Just (i, bug)
+  Nothing -> Nothing
+  _ -> error "impossible"
+
+bottomEmpty :: Room -> Maybe Int
+bottomEmpty rm = go Nothing $ assocs rm where
+  go _ ((i, Nothing):rest) = go (Just i) rest
+  go dst _ = dst
+
+-- TODO: split into roomToRoomSources, roomToHallSources?
+roomSources :: Rooms -> [(Int, Int, Bug)]
+roomSources rms = do
+  (i, rm) <- assocs rms
+  (j, bug) <- maybeToList $ topOccupied rm
+  let (_, btm) = bounds rm
+      allowsLeaving k = case rm ! k of
+        Nothing -> True
+        Just bug' -> bugRoom bug' /= j
+  guard $ bugRoom bug /= j || any allowsLeaving [j + 1..btm]
+  pure (i, j, bug)
+
+roomTargets :: Bug -> Rooms -> [(Int, Int)]
+roomTargets b rms = do
+  let i = bugRoom b
+  j <- maybeToList $ bottomEmpty $ rms ! i
+  pure (i, j)
+
 validMoves :: World -> [(Int, World)]
 validMoves (rms, hall) =
-  roomTopToBottom <>
-  roomTopToTop <>
-  roomBottomToBottom <>
-  roomBottomToTop <>
-  hallwayBugToRoomBottom <>
-  hallwayBugToRoomTop <>
-  roomTopBugToHall <>
-  roomBottomBugToHall
+  roomToRoomMoves <>
+  roomToHallMoves <>
+  hallToRoomMoves
   where
-    roomTopToTop = do
-      (i, (Just bug, other1)) <- assocs rms
-      let j = bugRoom bug
-      guard $ i /= j
-      case rms ! j of
-        (Nothing, other2) -> do
-          guard $ clearBetween hall (roomToHall i) (roomToHall j)
-          guard $ other2 == Nothing || other2 == Just bug
-          let steps = 2 + abs (roomToHall i - roomToHall j)
-          pure ( energy bug * steps
-               , (rms // [(i, (Nothing, other1)), (j, (Just bug, other2))], hall)
-               )
-        _ -> empty
+    roomToRoomMoves = do
+      (i, j, bug) <- roomSources rms
+      (i', j') <- roomTargets bug rms
+      guard $ i /= i'
+      guard $ clearBetween hall (roomToHall i) (roomToHall i')
+      let steps = 2 + j + j' + abs (roomToHall i' - roomToHall j')
+          src' = rms ! i // [(j, Nothing)]
+          dst' = rms ! i' // [(j', Just bug)]
+          rms' = rms // [(i, src'), (i', dst')]
+      pure (energy bug * steps, (rms', hall))
 
-    roomTopToBottom = do
-      (i, (Just bug, other)) <- assocs rms
-      let j = bugRoom bug
-      guard $ i /= j
-      case rms ! j of
-        (Nothing, Nothing) -> do
-          guard $ clearBetween hall (roomToHall i) (roomToHall j)
-          let steps = 3 + abs (roomToHall i - roomToHall j)
-          pure ( energy bug * steps
-               , (rms // [(i, (Nothing, other)), (j, (Nothing, Just bug))], hall)
-               )
-        _ -> empty
+    roomToHallMoves = do
+      (i, j, bug) <- roomSources rms
+      k <- hallTargets hall i
+      let steps = 1 + j + abs (roomToHall i - k)
+          src' = rms ! i // [(j, Nothing)]
+          rms' = rms // [(i, src')]
+          hall' = hall // [(k, Just bug)]
+      pure (energy bug * steps, (rms', hall'))
 
-    roomBottomToTop = do
-      (i, (Nothing, Just bug)) <- assocs rms
-      let j = bugRoom bug
-      guard $ i /= j
-      case rms ! j of
-        (Nothing, other) -> do
-          guard $ clearBetween hall (roomToHall i) (roomToHall j)
-          guard $ other == Nothing || other == Just bug
-          let steps = 3 + abs (roomToHall i - roomToHall j)
-          pure ( energy bug * steps
-               , (rms // [(i, (Nothing, Nothing)), (j, (Just bug, other))], hall)
-               )
-        _ -> empty
-
-    roomBottomToBottom = do
-      (i, (Nothing, Just bug)) <- assocs rms
-      let j = bugRoom bug
-      guard $ i /= j
-      case rms ! j of
-        (Nothing, Nothing) -> do
-          guard $ clearBetween hall (roomToHall i) (roomToHall j)
-          let steps = 4 + abs (roomToHall i - roomToHall j)
-          pure ( energy bug * steps
-               , (rms // [(i, (Nothing, Nothing)), (j, (Nothing, Just bug))], hall)
-               )
-        _ -> empty
-
-    roomTopBugToHall = do
-      (i, (Just bug, other)) <- assocs rms
-      guard $ i /= bugRoom bug || other /= Just bug
-      j <- hallTargets hall i
-      let steps = 1 + abs (roomToHall i - j)
-      pure ( energy bug * steps
-           , (rms // [(i, (Nothing, other))], hall // [(j, Just bug)])
-           )
-
-    roomBottomBugToHall = do
-      (i, (Nothing, Just bug)) <- assocs rms
-      guard $ i /= bugRoom bug
-      j <- hallTargets hall i
-      let steps = 2 + abs (roomToHall i - j)
-      pure ( energy bug * steps
-           , (rms // [(i, (Nothing, Nothing))], hall // [(j, Just bug)])
-           )
-
-    hallwayBugToRoomTop = do
-      (j, Just bug) <- assocs hall
-      let i = bugRoom bug
-      case rms ! i of
-        (Nothing, other) -> do
-          guard $ clearBetween1 hall j (roomToHall i)
-          guard $ other == Just bug || other == Nothing -- must not be different bug
-          let steps = 1 + abs (roomToHall i - j)
-          pure ( energy bug * steps
-               , (rms // [(i, (Just bug, other))], hall // [(j, Nothing)])
-               )
-        _ -> empty
-
-    hallwayBugToRoomBottom = do
-      (j, Just bug) <- assocs hall
-      let i = bugRoom bug
-      case rms ! i of
-        (Nothing, Nothing) -> do
-          guard $ clearBetween1 hall j (roomToHall i)
-          let steps = 2 + abs (roomToHall i - j)
-          pure ( energy bug * steps
-               , (rms // [(i, (Nothing, Just bug))], hall // [(j, Nothing)])
-               )
-        _ -> empty
+    hallToRoomMoves = do
+      (k, Just bug) <- assocs hall
+      (i, j) <- roomTargets bug rms
+      guard $ clearBetween1 hall k (roomToHall i)
+      let steps = 1 + j + abs (k - roomToHall i)
+          dst' = rms ! i // [(j, Just bug)]
+          rms' = rms // [(i, dst')]
+          hall' = hall // [(k, Nothing)]
+      pure (energy bug * steps, (rms', hall'))
 
 heuristic :: World -> Int
-heuristic (rms, hall) = sum topBugs + sum bottomBugs + sum hallBugs where
-  topBugs = do
-    (i, (Just bug, _)) <- assocs rms
-    let j = bugRoom bug
-    pure $ energy bug * (2 + abs (roomToHall i - roomToHall j))
-
-  bottomBugs = do
-    (i, (_, Just bug)) <- assocs rms
-    let j = bugRoom bug
-    pure $ energy bug * (3 + abs (roomToHall i - roomToHall j))
+heuristic (rms, hall) = sum roomBugs + sum hallBugs where
+  roomBugs = do
+    (i, rm) <- assocs rms
+    (j, Just bug) <- assocs rm
+    let i' = bugRoom bug
+    pure $ if i /= i'
+      then energy bug * (2 + j + abs (roomToHall i - roomToHall i'))
+      else 0
 
   hallBugs = do
     (i, Just bug) <- assocs hall
@@ -196,10 +152,10 @@ heuristic (rms, hall) = sum topBugs + sum bottomBugs + sum hallBugs where
 
 won :: World -> Bool
 won (rms, _) =
-  rms ! 0 == (Just A, Just A) &&
-  rms ! 1 == (Just B, Just B) &&
-  rms ! 2 == (Just C, Just C) &&
-  rms ! 3 == (Just D, Just D)
+  all (== Just A) (rms ! 0) &&
+  all (== Just B) (rms ! 1) &&
+  all (== Just C) (rms ! 2) &&
+  all (== Just D) (rms ! 3)
 
 bugKind :: Parser Bug
 bugKind = A <$ "A" <|> B <$ "B" <|> C <$ "C" <|> D <$ "D"
@@ -227,10 +183,10 @@ rooms = do
   pod7 <- bugKind
   _ <- lexeme "#"
   _ <- lexeme "#########"
-  pure $ listArray (0, 3) [ (Just pod0, Just pod4)
-                          , (Just pod1, Just pod5)
-                          , (Just pod2, Just pod6)
-                          , (Just pod3, Just pod7)
+  pure $ listArray (0, 3) [ listArray (0, 1) [Just pod0, Just pod4]
+                          , listArray (0, 1) [Just pod1, Just pod5]
+                          , listArray (0, 1) [Just pod2, Just pod6]
+                          , listArray (0, 1) [Just pod3, Just pod7]
                           ]
 
 main :: IO ()
@@ -262,7 +218,11 @@ main = do
   -}
 
   {-
-  let world = (array (0,3) [(0,(Just B,Just A)),(1,(Just C,Just D)),(2,(Just B,Just C)),(3,(Just D,Just A))],array (0,10) [(0,Nothing),(1,Nothing),(2,Nothing),(3,Nothing),(4,Nothing),(5,Nothing),(6,Nothing),(7,Nothing),(8,Nothing),(9,Nothing),(10,Nothing)])
+  let unfuck (rms, hall) = (fmap unfuck' rms, hall)
+      unfuck' :: (Maybe Bug, Maybe Bug) -> Array Int (Maybe Bug)
+      unfuck' (shit, fuck) = listArray (0,1) [shit, fuck]
+      world :: World
+      world = unfuck $ (array (0,3) [(0,(Just B,Just A)),(1,(Just C,Just D)),(2,(Nothing,Just C)),(3,(Just D,Just A))],array (0,10) [(0,Nothing),(1,Nothing),(2,Nothing),(3,Just B),(4,Nothing),(5,Nothing),(6,Nothing),(7,Nothing),(8,Nothing),(9,Nothing),(10,Nothing)])
   print $ validMoves world
   print $ heuristic world
   print $ costToWin world validMoves heuristic won
