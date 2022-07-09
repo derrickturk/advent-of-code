abe46541c5cc9daa227235cf3db54b2247d88657
Derrick W. Turk <dwt@terminusdatascience.com>
2022-07-08 23:18:45 -0500

    A proper day2 solution with intcaml.

---
+++ 2019/intcaml/bin/day2.ml
@@ -0,0 +1,35 @@
+open Intcaml.Intcode
+open Intcaml.Intcode_loader
+
+let part1 input =
+  let cpu = cpu_of_string_exn input in
+  cpu.mem.(1) <- 12;
+  cpu.mem.(2) <- 2;
+  match run cpu with
+    | Ok () -> print_endline (string_of_int (cpu.mem.(0)))
+    | Error e -> print_endline (show_error e)
+
+exception Done of int * int
+
+let part2 input =
+  let mem = mem_of_string_exn input in
+  try
+    for x = 0 to 100 do
+      for y = 0 to 100 do
+        let cpu = { ip = 0; mem = Array.copy mem } in
+        cpu.mem.(1) <- x;
+        cpu.mem.(2) <- y;
+        match run cpu with
+          | Ok () -> if cpu.mem.(0) = 19690720
+            then raise (Done (x, y))
+          | _ -> ()
+      done
+    done;
+    print_endline "couldn't find it!"
+  with
+    Done (x, y) -> print_endline (string_of_int (100 * x + y))
+
+let () =
+  let input = In_channel.(input_all stdin) in
+  part1 input;
+  part2 input
