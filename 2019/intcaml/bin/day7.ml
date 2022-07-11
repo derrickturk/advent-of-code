055cfe63937b392e9ff7c149bc457e1a928db55f
Derrick W. Turk <dwt@terminusdatascience.com>
2022-07-10 21:30:27 -0500

    Day 7 done

--- 2019/intcaml/bin/day7.ml
+++ 2019/intcaml/bin/day7.ml
@@ -1,10 +1,21 @@
 open Intcaml
+open Intcode_loader
 
 module M = Machine.Make (Io.Lwt_mvar)
 
 exception Intcode_error of Intcaml.Error.t
 
-let thrust (p0, p1, p2, p3, p4) mem =
+let rec leaveOuts = function
+  | [] -> []
+  | hd::tl -> (hd, tl)::(List.map (fun (x, xs) -> (x, hd::xs)) (leaveOuts tl))
+
+let rec permute = function
+  | [] -> []
+  | [x] -> [[x]]
+  | xs -> List.concat (List.map permute1 (leaveOuts xs))
+and permute1 (hd, xs) = List.map (fun xs -> hd::xs) (permute xs)
+
+let thrust_part1 (p0, p1, p2, p3, p4) mem =
   let m0_in = Lwt_mvar.create p0 in
   let m1_in = Lwt_mvar.create p1 in
   let m2_in = Lwt_mvar.create p2 in
@@ -30,8 +41,46 @@ let thrust (p0, p1, p2, p3, p4) mem =
               ]
   in Lwt.bind (Lwt.join m_all) (fun _ -> Lwt_mvar.take m4_out)
 
+let thrust_part2 (p0, p1, p2, p3, p4) mem =
+  let loop = Lwt_mvar.create p0 in
+  let m1_in = Lwt_mvar.create p1 in
+  let m2_in = Lwt_mvar.create p2 in
+  let m3_in = Lwt_mvar.create p3 in
+  let m4_in = Lwt_mvar.create p4 in
+  let open Io.Lwt_mvar in
+  let m0_io = { input = loop; output = m1_in } in
+  let m1_io = { input = m1_in; output = m2_in } in
+  let m2_io = { input = m2_in; output = m3_in } in
+  let m3_io = { input = m3_in; output = m4_in } in
+  let m4_io = { input = m4_in; output = loop } in
+  let run io = let open Lwt_result in
+    get_exn (map_error (fun e -> Intcode_error e)
+      (M.run { ip = 0; mem = Array.copy mem } io))
+  in
+  let m_all = [ run m0_io
+              ; run m1_io
+              ; run m2_io
+              ; run m3_io
+              ; run m4_io
+              ; Lwt_mvar.put loop 0
+              ]
+  in Lwt.bind (Lwt.join m_all) (fun _ -> Lwt_mvar.take loop)
+
+let max_thrust f settings mem =
+  let max_thrust t = function
+    | [p0; p1; p2; p3; p4] ->
+        Lwt.map (fun t' -> max t t') (f (p0, p1, p2, p3, p4) mem)
+    | _ -> raise (Invalid_argument "programmer error")
+  in
+  Lwt_list.fold_left_s max_thrust 0 (permute settings)
+
 let () =
-  let init = (4, 3, 2, 1, 0) in
-  let mem = [|3;15;3;16;1002;16;10;16;1;16;15;15;4;15;99;0;0|] in
-  let t = Lwt_main.run (thrust init mem) in
-  print_endline (string_of_int t)
+  let input = In_channel.(input_all stdin) in
+  let mem = mem_of_string_exn input in
+  let (t1, t2) = Lwt_main.run @@
+    Lwt.both 
+      (max_thrust thrust_part1 [0; 1; 2; 3; 4] mem)
+      (max_thrust thrust_part2 [5; 6; 7; 8; 9] mem)
+  in
+  print_endline (string_of_int t1);
+  print_endline (string_of_int t2)
