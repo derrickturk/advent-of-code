2a9d7c7196b6d677796cd02072481a327f0dc061
Derrick W. Turk <dwt@terminusdatascience.com>
2022-11-17 14:34:31 -0600

    C U K E S

--- 2021/25/SeaFloor.hs
+++ 2021/25/SeaFloor.hs
@@ -9,6 +9,9 @@ module SeaFloor
   , southies
   , empty
   , fromLines
+  , occupied
+  , step
+  , pprint
   ) where
 
 import Data.List (foldl')
@@ -36,3 +39,32 @@ fromLines = fromLines' 0 empty where
   withElem j f (i, 'v') =
     f { width = i + 1, height = j + 1, southies = S.insert (i, j) f.southies }
   withElem j f (i, _) = f { width = i + 1, height = j + 1 }
+
+occupied :: Pos -> SeaFloor -> Bool
+occupied p f = S.member p f.easties || S.member p f.southies
+
+step :: SeaFloor -> SeaFloor
+step = stepSouthies . stepEasties
+
+pprint :: SeaFloor -> String
+pprint f = unlines $
+  [[renderPos (i, j) | i <- [0..(f.width - 1)]] | j <- [0..(f.height - 1)]]
+  where renderPos p = if S.member p f.easties
+                        then '>'
+                        else if S.member p f.southies
+                          then 'v'
+                          else '.'
+
+stepEasties :: SeaFloor -> SeaFloor
+stepEasties f = f { easties = S.map step1E f.easties } where
+  step1E (i, j) = let i' = (i + 1) `mod` f.width
+                   in if (i', j) `occupied` f
+                        then (i, j)
+                        else (i', j)
+
+stepSouthies :: SeaFloor -> SeaFloor
+stepSouthies f = f { southies = S.map step1S f.southies } where
+  step1S (i, j) = let j' = (j + 1) `mod` f.height
+                   in if (i, j') `occupied` f
+                        then (i, j)
+                        else (i, j')
--- 2021/25/cucurbiform.hs
+++ 2021/25/cucurbiform.hs
@@ -1,6 +1,20 @@
 import qualified SeaFloor as SF
 
+stepsToStop :: SF.SeaFloor -> Int
+stepsToStop = go 0 where
+  go n f = let f' = SF.step f
+               n' = n + 1
+            in if f == f' then n' else go n' f'
+
+renderSteps :: SF.SeaFloor -> Int -> IO ()
+renderSteps _ 0 = pure ()
+renderSteps f n = do
+  putStrLn (SF.pprint f)
+  putStrLn ""
+  renderSteps (SF.step f) (n - 1)
+
 main :: IO ()
 main = do
-  floor <- SF.fromLines . lines <$> getContents
-  print floor
+  sf <- SF.fromLines . lines <$> getContents
+  print $ stepsToStop sf
+  -- renderSteps sf 10
