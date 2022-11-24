c4f5e0258485b4cc1ffafc68da8cd9aaf7d742ea
Derrick W. Turk <dwt@terminusdatascience.com>
2022-11-23 22:50:24 -0600

    that was dumb

--- 2019/16/fft.hs
+++ 2019/16/fft.hs
@@ -19,6 +19,9 @@ patternFor n =
   replicate (n + 1) 0 <>
   replicate (n + 1) (-1)
 
+fastBackHalf :: [Int] -> [Int]
+fastBackHalf = fmap (`mod` 10) . scanr1 (+)
+
 decode :: String -> [Int]
 decode = fmap digitToInt
 
@@ -29,3 +32,9 @@ main :: IO ()
 main = do
   input <- decode <$> getLine
   putStrLn $ encode $ take 8 $ iterate fft input !! 100
+  let offset = read $ take 7 $ encode input
+  if offset < (length input * 10000) `div` 2
+    then putStrLn "that dog won't hunt"
+    else do
+      let input' = drop offset $ concat $ replicate 10000 input
+      putStrLn $ encode $ take 8 $ iterate fastBackHalf input' !! 100
