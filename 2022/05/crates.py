7bef882c15fb7f7828cc1907aa2e53a0d12d4ea4
Derrick W. Turk <dwt@terminusdatascience.com>
2022-12-05 13:49:03 -0600

    and part 2

--- 2022/05/crates.py
+++ 2022/05/crates.py
@@ -1,4 +1,5 @@
 import sys
+from copy import deepcopy
 from enum import Enum, auto
 
 class ParseState:
@@ -17,6 +18,7 @@ def main() -> int:
     state = ParseState.Initial
     n_fields: int = -1
     stacks: list[list[str]] = []
+    stacks2: list[list[str]] = []
 
     for line in sys.stdin:
         line = line.rstrip('\r\n')
@@ -33,15 +35,20 @@ def main() -> int:
                 update_stacks(line, stacks)
             case ParseState.Skip:
                 state = ParseState.ReadMoves
+                stacks2 = deepcopy(stacks)
                 continue
             case ParseState.ReadMoves:
                 _, count, _, src, _, dst = line.split(' ')
                 dst0 = int(dst) - 1
                 src0 = int(src) - 1
-                for _ in range(int(count)):
+                i_count = int(count)
+                for _ in range(i_count):
                     stacks[dst0].append(stacks[src0].pop())
+                stacks2[dst0] += stacks2[src0][-i_count:]
+                stacks2[src0] = stacks2[src0][:-i_count]
 
     print(''.join(s[-1] for s in stacks))
+    print(''.join(s[-1] for s in stacks2))
 
     return 0
 
