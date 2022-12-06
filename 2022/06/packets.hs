796a34e6a41213bd17c3632d678b6c7886affaca
Derrick W. Turk <dwt@terminusdatascience.com>
2022-12-05 23:14:10 -0600

    cleanup

--- 2022/06/packets.hs
+++ 2022/06/packets.hs
@@ -1,21 +1,15 @@
 import Data.List (nub)
 
-untilSignal :: [Char] -> Maybe Int
-untilSignal = go 0 where
-  go n (a:rest@(b:c:d:_))
-    | length (nub [a, b, c, d]) == 4 = Just (n + 4)
+untilSignal :: Eq a => Int -> [a] -> Maybe Int
+untilSignal k = go 0 where
+  go _ [] = Nothing
+  go n xs@(_:rest)
+    | length (nub $ take k xs) == k = Just (n + k)
     | otherwise = go (n + 1) rest
-  go _ _ = Nothing
 
-untilSignal14 :: [Char] -> Maybe Int
-untilSignal14 = go 0 where
-  go count (a:rest@(b:c:d:e:f:g:h:i:j:k:l:m:n:_))
-    | length (nub [a, b, c, d, e, f, g, h, i, j, k, l, m, n]) == 14 = Just (count + 14)
-    | otherwise = go (count + 1) rest
-  go _ _ = Nothing
 
 main :: IO ()
 main = do
   input <- getLine
-  print $ untilSignal input
-  print $ untilSignal14 input
+  print $ untilSignal 4 input
+  print $ untilSignal 14 input
