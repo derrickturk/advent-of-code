db9c3cd15ccd9dd3ea7e0789a6447da9eeb0ef89
Derrick W. Turk <dwt@terminusdatascience.com>
2023-12-02 15:26:03 -0600

    sick

--- 2023/02/dice.hs
+++ 2023/02/dice.hs
@@ -11,6 +11,16 @@ infix 4 <:
 (<:) :: Dice -> Dice -> Bool
 (Dice r1 g1 b1) <: (Dice r2 g2 b2) = r1 <= r2 && g1 <= g2 && b1 <= b2
 
+-- you need to be dicemaxxing
+instance Semigroup Dice where
+  (Dice r1 g1 b1) <> (Dice r2 g2 b2) = Dice (max r1 r2) (max g1 g2) (max b1 b2)
+
+instance Monoid Dice where
+  mempty = Dice 0 0 0
+
+power :: Dice -> Int
+power (Dice r g b) = r * g * b
+
 color :: Parser (Int -> Dice -> Dice)
 color =  ((\v d -> d { red = v }) <$ "red")
      <|> ((\v d -> d { green = v }) <$ "green")
@@ -26,7 +36,7 @@ entry = do
 reveal :: Parser Dice
 reveal = do
   entries <- sepBy ", " entry
-  return $ foldl' (flip ($)) (Dice 0 0 0) entries
+  return $ foldl' (flip ($)) mempty entries
 
 game :: Parser (Int, [Dice])
 game = do
@@ -40,3 +50,4 @@ main :: IO ()
 main = do
   games <- parseStdin (some (lexeme' game))
   print $ (sum . fmap fst . filter (all (<: Dice 12 13 14) . snd)) <$> games
+  print $ (sum . fmap (power . mconcat . snd)) <$> games
