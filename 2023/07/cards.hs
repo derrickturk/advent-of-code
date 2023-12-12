31859f04ef5214c67c0294552332b6ba46b1b790
Derrick W. Turk <dwt@terminusdatascience.com>
2023-12-12 15:17:31 -0600

    abstract

--- 2023/07/cards.hs
+++ 2023/07/cards.hs
@@ -13,10 +13,10 @@ data Card
   | A
   deriving (Show, Eq, Ord)
 
-newtype Hand = Hand { unHand {- me, you brigand -} :: (M.Map Card Int, [Card]) }
+newtype Hand c = Hand { unHand {- me, you brigand -} :: (M.Map c Int, [c]) }
   deriving (Show, Eq)
 
-makeHand :: [Card] -> Hand
+makeHand :: Ord c => [c] -> Hand c
 makeHand cs =
   let bump Nothing = Just 1
       bump (Just c) = Just (c + 1)
@@ -33,17 +33,20 @@ data HandKind
   | Five
   deriving (Show, Eq, Ord)
 
-classify :: Hand -> HandKind
-classify (Hand (m, _)) = case sort (M.elems m) of
-  [5] -> Five
-  [1, 4] -> Four
-  [2, 3] -> FullHouse
-  [1, 1, 3] -> Three
-  [1, 2, 2] -> TwoPair
-  [1, 1, 1, 2] -> OnePair
-  _ -> HighCard
+class Classify c where
+  classify :: Hand c -> HandKind
 
-instance Ord Hand where
+instance Classify Card where
+  classify (Hand (m, _)) = case sort (M.elems m) of
+    [5] -> Five
+    [1, 4] -> Four
+    [2, 3] -> FullHouse
+    [1, 1, 3] -> Three
+    [1, 2, 2] -> TwoPair
+    [1, 1, 1, 2] -> OnePair
+    _ -> HighCard
+
+instance (Ord c, Classify c) => Ord (Hand c) where
   compare a@(Hand (_, acs)) b@(Hand (_, bcs)) =
     case compare (classify a) (classify b) of
       EQ -> compare acs bcs
@@ -60,20 +63,45 @@ parseCard c
                  in if v >= 2 && v <= 9 then Just (Number v) else Nothing
   | otherwise = Nothing
 
-parseHand :: String -> Maybe Hand
+parseHand :: String -> Maybe (Hand Card)
 parseHand = fmap makeHand . traverse parseCard
 
-parseHandBid :: String -> Maybe (Hand, Int)
+parseHandBid :: String -> Maybe (Hand Card, Int)
 parseHandBid l = do
   [hand, bid] <- pure $ words l
   hand' <- parseHand hand
   bid' <- readMaybe bid
   return (hand', bid')
 
-score :: [(Hand, Int)] -> Int
+score :: (Ord c, Classify c) => [(Hand c, Int)] -> Int
 score = sum . fmap (\(r, (_, b)) -> r * b) . zip [1..] . sort
+
+newtype JCard = JCard { unJCard :: Card }
+  deriving (Show, Eq)
+
+instance Ord JCard where
+  compare (JCard J) (JCard J) = EQ
+  compare (JCard J) _ = LT
+  compare _ (JCard J) = GT
+  compare (JCard c1) (JCard c2) = compare c1 c2
+
+instance Classify JCard where
+  classify (Hand (m, _)) =
+    case sort (M.elems $ M.filterWithKey (\k _ -> k /= JCard J) m) of
+     [] -> Five
+     [_] -> Five
+     [1, _] -> Four
+     [2, _] -> FullHouse
+     [1, 1, _] -> Three
+     [1, 2, 2] -> TwoPair
+     [1, 1, 1, _] -> OnePair
+     _ -> HighCard
+
+jokerize :: Hand Card -> Hand JCard
+jokerize (Hand (_, cs)) = makeHand $ JCard <$> cs
 
 main :: IO ()
 main = do
   Just hands <- traverse parseHandBid . lines <$> getContents
   print $ score hands
+  print $ score $ (\(h, b) -> (jokerize h, b)) <$> hands
