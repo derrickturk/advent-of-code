f692c57a78cd3e15f41311beef999ffae3f9bcba
Derrick W. Turk <dwt@terminusdatascience.com>
2024-12-04 22:58:17 -0600

    day 3 done; ugh threshold exceeded for day 4

--- 2024/03/searchy.hs
+++ 2024/03/searchy.hs
@@ -1,26 +1,41 @@
 import Data.Char (isDigit)
 import Data.List (span)
 
-data Mul = Mul Int Int
+data Op
+  = Mul Int Int
+  | Do
+  | Dont
   deriving Show
 
-eval :: Mul -> Int
-eval (Mul x y) = x * y
+eval1 :: [Op] -> Int
+eval1 [] = 0
+eval1 (Mul x y:rest) = x * y + eval1 rest
+eval1 (_:rest) = eval1 rest
 
-muls :: String -> [Mul]
-muls "" = []
-muls ('m':'u':'l':'(':rest) =
+eval2 :: [Op] -> Int
+eval2 = go True where
+  go _ [] = 0
+  go _ (Do:rest) = go True rest
+  go _ (Dont:rest) = go False rest
+  go True (Mul x y:rest) = x * y + go True rest
+  go False (_:rest) = go False rest
+
+parse :: String -> [Op]
+parse "" = []
+parse ('m':'u':'l':'(':rest) =
   case span isDigit rest of
-    ("", _) -> muls rest
+    ("", _) -> parse rest
     (num1, ',':rest') -> case span isDigit rest' of
-      ("", _) -> muls rest'
-      (num2, ')':rest'') -> Mul (read num1) (read num2) : muls rest''
-      (_, rest'') -> muls rest''
-    (_, rest') -> muls rest'
-muls (_:rest) = muls rest
+      ("", _) -> parse rest'
+      (num2, ')':rest'') -> Mul (read num1) (read num2) : parse rest''
+      (_, rest'') -> parse rest''
+    (_, rest') -> parse rest'
+parse ('d':'o':'(':')':rest) = Do : parse rest
+parse ('d':'o':'n':'\'':'t':'(':')':rest) = Dont : parse rest
+parse (_:rest) = parse rest
 
 main :: IO ()
 main = do
-  code <- muls <$> getContents
-  print code
-  print $ sum $ eval <$> code
+  code <- parse <$> getContents
+  print $ eval1 code
+  print $ eval2 code
