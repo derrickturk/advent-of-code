7ca60b0d09ecf2ce257d870bab911829d10522f4
Derrick W. Turk <dwt@terminusdatascience.com>
2024-12-04 22:38:07 -0600

    hmm

--- 2024/02/unsafe.hs
+++ 2024/02/unsafe.hs
@@ -14,7 +14,15 @@ safe xs =
   let ds = deltas xs
    in all (> 0) ds && all (< 4) ds || all (< 0) ds && all (> (-4)) ds
 
+leaveOneOut :: [a] -> [[a]]
+leaveOneOut [] = []
+leaveOneOut (x:xs) = xs:[x:ys | ys <- leaveOneOut xs]
+
+safeish :: Report -> Bool
+safeish xs = safe xs || any safe (leaveOneOut xs)
+
 main :: IO ()
 main = do
   Just reports <- traverse parseReport . lines <$> getContents
   print $ length $ filter safe reports
+  print $ length $ filter safeish reports
