22535d5a740e7fb5830405d13f22f1a4852e5003
Derrick W. Turk <dwt@terminusdatascience.com>
2025-12-01 12:43:25 -0600

    math isn't fun, what the fuck is this shit

--- 2025/01/safe.hs
+++ 2025/01/safe.hs
@@ -13,6 +13,20 @@ rotate :: Int -> Rotation -> Int
 rotate n (Left, m) = (n - m) `mod` 100
 rotate n (Right, m) = (n + m) `mod` 100
 
+countingRotate :: Int -> Rotation -> (Int, Int)
+countingRotate n (Left, m) = countingRotate' n (-m)
+countingRotate n (Right, m) = countingRotate' n m
+
+countingRotate' :: Int -> Int -> (Int, Int)
+countingRotate' n m =
+  case (n + m) `divMod` 100 of
+    (wraps, 0)
+      | wraps >= 1 -> (0, wraps)
+      | otherwise -> (0, 1 + abs wraps)
+    (wraps, n')
+      | n == 0 && wraps < 0 -> (n', abs wraps - 1)
+      | otherwise -> (n', abs wraps)
+
 parseRotation :: String -> Maybe Rotation
 parseRotation [] = Nothing
 parseRotation (dir:rest) = do
@@ -24,7 +38,14 @@ parseRotation (dir:rest) = do
   guard (dist >= 0)
   pure (dir', dist)
 
+countingZeros :: Int -> [Rotation] -> Int
+countingZeros = go 0 where
+  go acc _ [] = acc
+  go acc n (r:rest) =
+    let (n', zeros) = countingRotate n r in go (acc + zeros) n' rest
+
 main :: IO ()
 main = do
   Just rots <- traverse parseRotation . lines <$> getContents
   print $ length $ filter (== 0) $ scanl rotate 50 rots
+  print $ countingZeros 50 rots
