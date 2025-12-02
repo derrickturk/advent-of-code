af1eaaab39db695d5a6e332ffd97796ac3d5772d
Derrick W. Turk <dwt@terminusdatascience.com>
2025-12-02 10:34:45 -0600

    part 1

---
+++ 2025/02/repeat.hs
@@ -0,0 +1,45 @@
+-- import Data.Maybe (isJust)
+import Text.Read (readMaybe)
+
+{- this actually solves a much more difficult problem!
+repeated :: String -> Maybe String
+repeated [] = Nothing
+repeated [_] = Nothing
+repeated s =
+  let stringLen = length s
+      maxSeqLen = stringLen `div` 2
+      trySeqLens = [1..maxSeqLen]
+      go [] = Nothing
+      go (n:rest) =
+        let (prefix, suffix) = splitAt n s
+            expected = concat $ replicate (stringLen `div` n - 1) prefix
+         in if stringLen `mod` n == 0 && suffix == expected
+              then Just prefix
+              else go rest
+   in go trySeqLens
+-}
+
+repeated2 :: String -> Bool
+repeated2 s = case length s `divMod` 2 of
+  (0, _) -> False
+  (half, 0) -> let (pre, suf) = splitAt half s in pre == suf
+  (_, _) -> False
+
+repeatedInRange :: Int -> Int -> [Int]
+repeatedInRange m n = filter (repeated2 . show) [m..n]
+
+split :: Eq a => a -> [a] -> [[a]]
+split _ [] = []
+split d xs = case break (== d) xs of
+  (pre, []) -> [pre]
+  (pre, _:suf) -> pre:split d suf
+
+parseRanges :: String -> Maybe [(Int, Int)]
+parseRanges = traverse (parseRange . split '-') . split ',' where
+  parseRange [x, y] = (,) <$> readMaybe x <*> readMaybe y
+  parseRange _ = Nothing
+
+main :: IO ()
+main = do
+  Just ranges <- parseRanges <$> getContents
+  print $ sum $ concatMap (uncurry repeatedInRange) ranges
