096c5b2b4bd6bfd6df900a3a28942b17bf30fc5e
Derrick W. Turk <dwt@terminusdatascience.com>
2025-12-07 13:08:33 -0600

    part 1

---
+++ 2025/07/tele.hs
@@ -0,0 +1,69 @@
+import Control.Monad (foldM)
+import qualified Data.Set as S
+
+data Manifold = Manifold Int (Int, Int) (S.Set (Int, Int)) deriving Show
+
+data Move
+  = Continue (Int, Int)
+  | Split (Int, Int) (Int, Int)
+  | Fizzle
+  deriving Show
+
+enumerate2d :: [[a]] -> [((Int, Int), a)]
+enumerate2d rows = [ ((i, j), x)
+                   | (i, row) <- zip [0..] rows
+                   , (j, x) <- zip [0..] row
+                   ]
+
+parse :: [String] -> Maybe Manifold
+parse rows = do
+  (maxI, origins, splitters) <-
+    foldM accum (0, S.empty, S.empty) (enumerate2d rows)
+  case S.elems origins of
+    [o] -> pure $ Manifold maxI o splitters
+    _ -> Nothing
+  where
+    accum (maxI, origins, splitters) ((i, _), '.') =
+      pure (max i maxI, origins, splitters)
+    accum (maxI, origins, splitters) (ix@(i, _), 'S') =
+      pure (max i maxI, S.insert ix origins, splitters)
+    accum (maxI, origins, splitters) (ix@(i, _), '^') =
+      pure (max i maxI, origins, S.insert ix splitters)
+    accum _ _ = Nothing
+
+moves :: Manifold -> S.Set (Int, Int) -> [Move]
+moves (Manifold maxI _ splitters) = fmap move . S.toList where
+  move (i, j)
+    | i + 1 > maxI = Fizzle
+    | (i + 1, j) `S.member` splitters = Split (i + 1, j + 1) (i + 1, j - 1)
+    | otherwise = Continue (i + 1, j)
+
+newPositions :: [Move] -> S.Set (Int, Int)
+newPositions = S.fromList . concatMap f where
+  f Fizzle = []
+  f (Continue ix) = [ix]
+  f (Split ix1 ix2) = [ix1, ix2]
+
+countSplits :: [Move] -> Int
+countSplits = go 0 where
+  go n [] = n
+  go n (Fizzle:rest) = go n rest
+  go n (Continue _:rest) = go n rest
+  go n (Split _ _:rest) = go (n + 1) rest
+
+start :: Manifold -> S.Set (Int, Int)
+start (Manifold _ o _) = S.singleton o
+
+runCountingSplits :: Manifold -> Int
+runCountingSplits m =
+  case dropWhile (not . S.null . snd) $ iterate f (0, start m) of
+    ((n, _):_) -> n
+    _ -> error "not possible"
+  where
+    f (n, beams) = let ms = moves m beams
+                    in (n + countSplits ms, newPositions ms)
+
+main :: IO ()
+main = do
+  Just mani <- parse . lines <$> getContents
+  print $ runCountingSplits mani
