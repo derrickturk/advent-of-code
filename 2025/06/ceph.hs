a6fbaf4cc3a9d4b47863bd640e2a584fe61bbfcb
Derrick W. Turk <dwt@terminusdatascience.com>
2025-12-06 20:34:39 -0600

    Todder on lap = -80 IQ points

--- 2025/06/ceph.hs
+++ 2025/06/ceph.hs
@@ -5,25 +5,41 @@ import Text.Read (readMaybe)
 data Op = Mul | Add deriving Show
 data Ceph = Ceph [Int] Op deriving Show
 
+eval :: Ceph -> Int
+eval (Ceph nums Add) = sum nums
+eval (Ceph nums Mul) = product nums
+
 transpose :: [[a]] -> [[a]]
 transpose = getZipList . traverse ZipList
+
+parseOp :: String -> Maybe Op
+parseOp "+" = Just Add
+parseOp "*" = Just Mul
+parseOp _ = Nothing
 
 parse :: String -> Maybe [Ceph]
 parse = traverse parseCeph . transpose . fmap words . lines where
   parseCeph ws = do
     (nums, op) <- unsnoc ws
-    nums' <- traverse readMaybe nums
-    op' <- case op of
-      "+" -> Just Add
-      "*" -> Just Mul
-      _ -> Nothing
-    pure $ Ceph nums' op'
+    Ceph <$> traverse readMaybe nums <*> parseOp op
 
-eval :: Ceph -> Int
-eval (Ceph nums Add) = sum nums
-eval (Ceph nums Mul) = product nums
+split :: (a -> Bool) -> [a] -> [[a]]
+split _ [] = []
+split p xs = case break p xs of
+  (pre, []) -> [pre]
+  (pre, _:suf) -> pre:split p suf
+
+parse' :: String -> Maybe [Ceph]
+parse' = traverse parseBlock . split (all (== ' ')) . transpose . lines where
+  parseBlock [] = Nothing
+  parseBlock (first:rest) = do
+    (num, op) <- unsnoc first
+    Ceph <$> traverse readMaybe (num:rest) <*> parseOp [op]
 
 main :: IO ()
 main = do
-  Just cephs <- parse <$> getContents
+  contents <- getContents
+  Just cephs <- pure $ parse contents
+  Just cephs' <- pure $ parse' contents
   print $ sum $ eval <$> cephs
+  print $ sum $ eval <$> cephs'
